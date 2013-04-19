0d7eb9afb9428d79d59af2099c47577821f084a6
Takuya Nishimoto <nishimotz@gmail.com>
2013-04-19 15:38:36 +0900

    work directory changed

--- jptools/update_dic.cmd
+++ jptools/update_dic.cmd
@@ -2,7 +2,7 @@ cd c:\work\nvda\miscdep\include\jtalk
 call setenv-x86.cmd
 call all-build.cmd
 call all-install.cmd
-python mecabRunner.py > c:\work\nvda\jp2013.1\jptools\__mecab_test.txt
-cd c:\work\nvda\jp2013.1\jptools
-copy c:\work\nvda\miscdep\source\synthDrivers\jtalk\dic\* c:\work\nvda\jp2013.1\source\synthDrivers\jtalk\dic
+python mecabRunner.py > c:\work\nvda\nvdajp\jptools\__mecab_test.txt
+cd c:\work\nvda\nvdajp\jptools
+copy c:\work\nvda\miscdep\source\synthDrivers\jtalk\dic\* c:\work\nvda\nvdajp\source\synthDrivers\jtalk\dic
 python jpBrailleRunner.py
