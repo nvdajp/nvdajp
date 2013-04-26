e51806277da8ea04289a0cc3f2d2c6f8f7ac2099
Takuya Nishimoto <nishimotz@gmail.com>
2013-04-27 01:30:58 +0900

    fixed typo

--- source/appModules/_skypeWin8Hack.py
+++ source/appModules/_skypeWin8Hack.py
@@ -20,24 +20,24 @@ class AppModule(appModuleHandler.AppModule):
 
 	_handler = None
 
-	def __threadAddAutometion(self, evtId):
+	def __threadAddAutomation(self, evtId):
 		ctypes.oledll.ole32.CoInitializeEx(None,comtypes.COINIT_MULTITHREADED)
 		self._handler.clientObject.AddAutomationEventHandler(evtId, self._handler.rootElement, TreeScope_Subtree, self._handler.baseCacheRequest, self._handler)
 
-	def __threadRemoveAutometion(self, evtId):
+	def __threadRemoveAutomation(self, evtId):
 		ctypes.oledll.ole32.CoInitializeEx(None,comtypes.COINIT_MULTITHREADED)
 		self._handler.clientObject.RemoveAutomationEventHandler(evtId, self._handler.rootElement, self._handler)
 
-	def _threadAddAutometion(self, evtId):
-		t = threading.Thread(target=self.__threadAddAutometion, args=(evtId,))
+	def _threadAddAutomation(self, evtId):
+		t = threading.Thread(target=self.__threadAddAutomation, args=(evtId,))
 		t.setDaemon(True)
 		t.start()
 		if t is not None:
 			if t.isAlive:
 				t.join(0.5)
 
-	def _threadRemoveAutometion(self, evtId):
-		t = threading.Thread(target=self.__threadRemoveAutometion, args=(evtId,))
+	def _threadRemoveAutomation(self, evtId):
+		t = threading.Thread(target=self.__threadRemoveAutomation, args=(evtId,))
 		t.setDaemon(True)
 		t.start()
 		if t is not None:
@@ -48,12 +48,12 @@ class AppModule(appModuleHandler.AppModule):
 		super(AppModule, self).__init__(*args, **kwargs)
 		if UIAHandler.isUIAAvailable:
 			self._handler = UIAHandler.handler
-			self._threadRemoveAutometion(UIAHandler.UIA_ToolTipOpenedEventId)
+			self._threadRemoveAutomation(UIAHandler.UIA_ToolTipOpenedEventId)
 
 	def terminate(self):
 		super(AppModule, self).terminate()
 		if UIAHandler.isUIAAvailable:
-			self._threadAddAutometion(UIAHandler.UIA_ToolTipOpenedEventId)
+			self._threadAddAutomation(UIAHandler.UIA_ToolTipOpenedEventId)
 			self._handler = None
 
 	def event_NVDAObject_init(self,obj):
