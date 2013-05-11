15a622d73ab6d003312f0a476a54f6fa29982b54
Takuya Nishimoto <nishimotz@gmail.com>
2013-05-11 18:51:31 +0900

    input help mode should annouce 'half-shape/full-shape' key correctly

--- source/keyLabels.py
+++ source/keyLabels.py
@@ -140,5 +140,9 @@ localizedKeyLabels = {
 	# Translators: This is the name of a key on the keyboard.
 	'IMEConvert': _("IME convert"),
 	# Translators: This is the name of a key on the keyboard.
+	'IMEChangeStatus2': _("toggle input method"),
+	# Translators: This is the name of a key on the keyboard.
+	'IMEChangeStatus3': _("toggle input method"),
+	# Translators: This is the name of a key on the keyboard.
 	'pause': _("pause"),
 }
