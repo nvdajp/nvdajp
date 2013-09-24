740af9cc33569b422841825b8b1015695642b077
Takuya Nishimoto <nishimotz@gmail.com>
2013-09-24 11:28:54 +0900

    refactored

--- source/synthDrivers/jtalk/_nvdajp_unicode.py
+++ source/synthDrivers/jtalk/_nvdajp_unicode.py
@@ -15,3 +15,11 @@ def unicode_normalize(s):
 	s = s.replace('\u00a5', '\\') # 0x00A5 YEN SIGN
 	s = s.replace('\u301c', '~')  # 0x301C WAVE DASH
 	return s
+
+def guessLang(msg, lang):
+	log.info(unicode(lang) + u':' + msg)
+	for i in xrange(len(msg)):
+		c = ord(msg[i])
+		if c > 0x7f:
+			return lang
+	return 'en'
