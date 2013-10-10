07bc4bd4236b05fe556599619354be3e0cd90e12
Takuya Nishimoto <nishimotz@gmail.com>
2013-10-10 11:24:44 +0900

    update check

--- jpUpdateCheck/index.php
+++ jpUpdateCheck/index.php
@@ -1,6 +1,6 @@
 <?php 
-define("LATEST_VERSION", "jpnext131005");
-define("LATEST_LAUNCHER", "https://dl.dropboxusercontent.com/u/62564469/nvda_jpnext131005.exe");
+define("LATEST_VERSION", "jpnext131010");
+define("LATEST_LAUNCHER", "https://dl.dropboxusercontent.com/u/62564469/nvda_jpnext131010.exe");
 $version = '';
 if (isset($_GET['version'])) {
 	$version = $_GET['version'];
