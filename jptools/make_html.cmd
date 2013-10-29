f6c74c5dcf67694dabef3ba2a4e90f766b260776
Takuya Nishimoto <nishimotz@gmail.com>
2013-10-29 18:48:38 +0900

    moved some files to miscdep.

--- jptools/make_html.cmd
+++
@@ -1,3 +0,0 @@
-python jpBrailleRunner.py -m
-python ..\txt2tags.py -t xhtml --toc __jpBrailleHarness.t2t
-python -c "import sys;lines = [line.strip() for line in sys.stdin.readlines()];import re;p = re.compile(r'<a href=\x22(mailto|http):[^>]+>([^<]+)</a>');print '\n'.join(map(lambda l:p.sub(r'\2', l), lines))" < __jpBrailleHarness.xhtml > c:\users\nishimotz\dropbox\public\jpBrailleHarness.xhtml
