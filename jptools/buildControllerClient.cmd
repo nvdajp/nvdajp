658c544c171883db319d586b6bdfb906147cd234
Takuya Nishimoto <nishimotz@gmail.com>
2014-01-17 18:58:06 +0900

    work regarding controller client. contains python test scripts by misono (nvdajp ti29342)

---
+++ jptools/buildControllerClient.cmd
@@ -0,0 +1,17 @@
+SET VERSION=nvdajp-client-140117
+SET ARGS=publisher=nvdajp release=1 version=%VERSION%
+cd ..
+call scons.bat nvdaHelper\client %ARGS%
+cd jptools
+cd nvdajpClient
+copy ..\..\build\x86\client\nvdaController.h client
+copy ..\..\build\x86\client\nvdaControllerClient32.dll client
+copy ..\..\build\x86\client\nvdaControllerClient32.exp client
+copy ..\..\build\x86\client\nvdaControllerClient32.lib client
+copy ..\..\build\x86_64\client\nvdaControllerClient64.dll client
+copy ..\..\build\x86_64\client\nvdaControllerClient64.exp client
+copy ..\..\build\x86_64\client\nvdaControllerClient64.lib client
+del /Q %VERSION%.zip
+7z a -xr!*~ -xr!.git* %VERSION%.zip client python
+cd ..
+
---
+++ jptools/nvdajpClient/python/test_speakSpelling.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# 日本語版で拡張した speakSpelling を追加した DLL は 
+# scons nvdaHelper\client で作成する必要があります。 
+# いちおう以下の Python コードで「カタカナ ひらがな」をピッチを変えて出力できていることは確認しました。 
+
+import time
+import ctypes
+DLLPATH = '../newClient/nvdaHelper/build/x86/client/nvdaControllerClient32.dll'
+clientLib=ctypes.windll.LoadLibrary(DLLPATH)
+res=clientLib.nvdaController_testIfRunning()
+if res!=0:
+	errorMessage=str(ctypes.WinError(res))
+	ctypes.windll.user32.MessageBoxW(0,u"Error: %s"%errorMessage,u"Error communicating with NVDA",0)
+for count in xrange(4):
+	clientLib.nvdaController_speakSpelling(u"カタカナ ひらがな")
+	time.sleep(5)
