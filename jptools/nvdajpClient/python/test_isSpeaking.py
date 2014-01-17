00e7c952554fbd0c9a0eb025534fc25b65e3468b
Takuya Nishimoto <nishimotz@gmail.com>
2014-01-17 20:27:13 +0900

    tested and updated for nvda 2013.3jp and Windows 8.1

--- jptools/nvdajpClient/python/test_isSpeaking.py
+++ jptools/nvdajpClient/python/test_isSpeaking.py
@@ -1,22 +1,21 @@
 # coding: utf-8
+from __future__ import unicode_literals
 import time
 import ctypes
-DLLPATH = '../newclient/nvdaHelper/build/x86/client/nvdaControllerClient32.dll'
+DLLPATH = r'..\client\nvdaControllerClient32.dll'
 clientLib=ctypes.windll.LoadLibrary(DLLPATH)
 res=clientLib.nvdaController_testIfRunning()
 if res!=0:
-	errorMessage=str(ctypes.WinError(res))
-	ctypes.windll.user32.MessageBoxW(0,u"Error: %s"%errorMessage,u"Error communicating with NVDA",0)
-	exit(1)
+	raise ctypes.WinError(res)
 clientLib.nvdaController_speakText(
-u"This is test case.\n \
-The case nvdaController_isSpeaking beep out when speaking with nvda! \
-")
-while ( True ):
+"""This is test case.
+The case nvdaController_isSpeaking beep out when speaking with nvda!
+""")
+while True:
 	time.sleep(0.5)
-	ctypes.windll.user32.MessageBeep(0)
+	ctypes.windll.kernel32.Beep(500, 100)
 	if not clientLib.nvdaController_isSpeaking():
 		break
-ctypes.windll.user32.MessageBeep(1)
+ctypes.windll.kernel32.Beep(1000, 100)
 clientLib.nvdaController_cancelSpeech()
-clientLib.nvdaController_speakText(u"Finished!")
+clientLib.nvdaController_speakText("Finished!")
