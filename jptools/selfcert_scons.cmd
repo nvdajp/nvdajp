4f9297326990bb731fb03bf5861b44c0081aa943
Takuya Nishimoto <nishimotz@gmail.com>
2014-04-28 17:23:56 +0900

    cleanup and added scripts for release

---
+++ jptools/selfcert_scons.cmd
@@ -0,0 +1,1 @@
+scons source user_docs launcher certFile=selfsigned.pfx publisher=nvdajp release=1 version=2014.1jp
---
+++ jptools/signed_output_pack.cmd
@@ -0,0 +1,1 @@
+7z a nvda-2014.1jp-140313-launcher-unsigned.zip output\nvda_2014.1jp.exe
