2869d6e53d93b727f308a0992ce10add80f50f99
Takuya Nishimoto <nishimotz@gmail.com>
2014-05-02 12:18:54 +0900

    reverted braille viewer to previous version. nvdajp ti33566

--- source/gui/brailleViewer.py
+++ source/gui/brailleViewer.py
@@ -9,15 +9,14 @@
 import wx
 import gui
 
-class brailleViewerFrame(wx.Frame):
+class brailleViewerFrame(wx.MiniFrame):
 
 	def __init__(self):
-		super(brailleViewerFrame, self).__init__(gui.mainFrame, wx.ID_ANY, _("NVDA Braille Viewer"), style=wx.CAPTION | wx.RESIZE_BORDER | wx.STAY_ON_TOP | wx.SYSTEM_MENU | wx.CLOSE_BOX | wx.MAXIMIZE_BOX)
+		super(brailleViewerFrame, self).__init__(gui.mainFrame, wx.ID_ANY, _("NVDA Braille Viewer"), style=wx.CAPTION | wx.RESIZE_BORDER)
 		self.Bind(wx.EVT_CLOSE, self.onClose)
-		self.SetFont(wx.Font(16,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_NORMAL,False,"DejaVu Sans"))
-		self.SetTransparent(int(255.0 * 0.90))
+		self.SetFont(wx.Font(20,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_NORMAL,False,"DejaVu Sans"))
 		sizer = wx.BoxSizer(wx.VERTICAL)
-		self.textCtrl = wx.TextCtrl(self, -1,size=(500,500),style=wx.TE_READONLY|wx.TE_MULTILINE)
+		self.textCtrl = wx.TextCtrl(self, -1,size=(600,200),style=wx.TE_READONLY|wx.TE_MULTILINE)
 		sizer.Add(self.textCtrl, proportion=1, flag=wx.EXPAND)
 		sizer.Fit(self)
 		self.SetSizer(sizer)
@@ -26,11 +25,6 @@ class brailleViewerFrame(wx.Frame):
 	def onClose(self, evt):
 		deactivate()
 		gui.mainFrame.sysTrayIcon.menu_tools_toggleBrailleViewer.Check(False)
-		return
-		if not evt.CanVeto():
-			self.Destroy()
-			return
-		evt.Veto()
 
 _guiFrame=None
 isActive=False
