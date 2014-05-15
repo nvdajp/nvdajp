8d6789141ca55ffc7e1df3f35a1f7c920d80af2c
Takuya Nishimoto <nishimotz@gmail.com>
2014-05-15 12:13:14 +0900

    update push destination

--- jptools/push_remote.cmd
+++ jptools/push_remote.cmd
@@ -1,5 +1,5 @@
-@rem SET BRANCH=jp2014.1
-SET BRANCH=jpbranch
+SET BRANCH=jp2014.2
+@rem SET BRANCH=jpbranch
 git push sfjp %BRANCH% --tags
 git push github %BRANCH% --tags
 git push bb %BRANCH% --tags
