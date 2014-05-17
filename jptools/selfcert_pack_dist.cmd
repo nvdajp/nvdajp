b547b22358af49234f89cb4ae73e95b1b0b5299d
Takuya Nishimoto <nishimotz@gmail.com>
2014-05-17 22:49:43 +0900

    build script updated

--- jptools/selfcert_pack_dist.cmd
+++ jptools/selfcert_pack_dist.cmd
@@ -1,2 +1,2 @@
-7z a nvda-2014.2jp-beta-140516-dist-self.zip dist\*.exe
-copy nvda-2014.2jp-beta-140516-dist-self.zip c:\users\nishimotz\dropbox\public\
+7z a nvda-2014.2jp-beta-140517-dist-self.zip dist\*.exe
+copy nvda-2014.2jp-beta-140517-dist-self.zip c:\users\nishimotz\dropbox\public\
