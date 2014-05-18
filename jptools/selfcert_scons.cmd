4d290c9504a425ae7091142fcfcd355be546194e
Takuya Nishimoto <nishimotz@gmail.com>
2014-05-18 11:54:44 +0900

    updated scripts

--- jptools/selfcert_scons.cmd
+++ jptools/selfcert_scons.cmd
@@ -1,1 +1,2 @@
-scons source user_docs launcher certFile=selfsigned.pfx publisher=nvdajp release=1 version=2014.2jp-beta
+SET VERSION=2014.2jp-beta
+scons source user_docs launcher certFile=selfsigned.pfx publisher=nvdajp release=1 version=%VERSION%
