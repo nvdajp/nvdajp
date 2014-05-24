e9c68374a95c8bbec8c25dfb6bd0ea6f76bb018d
Takuya Nishimoto <nishimotz@gmail.com>
2014-05-24 22:52:04 +0900

    building 2014.2jp

--- jptools/selfcert_pack_dist.cmd
+++ jptools/selfcert_pack_dist.cmd
@@ -1,4 +1,4 @@
-SET VERSION=2014.2jp-beta
-SET REV=140517
+SET VERSION=2014.2jp
+SET REV=140524
 7z a nvda-%VERSION%-%REV%-dist-self.zip dist\*.exe
 copy nvda-%VERSION%-%REV%-dist-self.zip c:\users\nishimotz\dropbox\public\
