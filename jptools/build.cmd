af996df3314faab326e16ec865215355fa692f12
Takuya Nishimoto <nishimotz@gmail.com>
2014-07-30 14:42:19 +0900

    build script should stop on error

--- jptools/build.cmd
+++ jptools/build.cmd
@@ -1,18 +1,57 @@
-@rem SET VERSION=2014.3jp
-SET BUILDTYPE=jpbeta
-SET VERSION=%BUILDTYPE%140730
+@rem set VERSION=2014.3jp
+set BUILDTYPE=jpbeta
+set VERSION=%BUILDTYPE%140730
+set PUBLISHER=nvdajp
+set PAUSE=0
+set CLEAN=1
+set CLIENT=1
 
-SET DEBUG=
-@rem SET DEBUG=nvdaHelperDebugFlags=noOptimize,RTC,debugCRT,symbols
-@rem SET DEBUG=nvdaHelperDebugFlags=symbols
+set DEBUG=
+@rem set DEBUG=nvdaHelperDebugFlags=noOptimize,RTC,debugCRT,symbols
+@rem set DEBUG=nvdaHelperDebugFlags=symbols
 
-SET ARGS=publisher=nvdajp release=1 version=%VERSION% %DEBUG%
+set ARGS=-j4 publisher=%PUBLISHER% release=1 version=%VERSION% %DEBUG%
+
 del /Q output\nvda_%VERSION%.exe
-call scons.bat -c
-call scons.bat source dist -j4 %ARGS%
-call scons.bat user_docs -j4 %ARGS%
-call scons.bat launcher %ARGS%
+@if not "%ERRORLEVEL%"=="0" goto onerror
 
+@if "%CLEAN%"=="0" goto skip_clean
+@if "%PAUSE%"=="1" pause
+call scons.bat -c
+@if not "%ERRORLEVEL%"=="0" goto onerror
+:skip_clean
+
+@if "%PAUSE%"=="1" pause
+call scons.bat user_docs %ARGS%
+@if not "%ERRORLEVEL%"=="0" goto onerror
+
+@if "%PAUSE%"=="1" pause
+call scons.bat source\locale %ARGS%
+@if not "%ERRORLEVEL%"=="0" goto onerror
+
+@if "%PAUSE%"=="1" pause
+call scons.bat source\comInterfaces %ARGS%
+@if not "%ERRORLEVEL%"=="0" goto onerror
+
+@if "%PAUSE%"=="1" pause
+call scons.bat NVDAHelper %ARGS%
+@if not "%ERRORLEVEL%"=="0" goto onerror
+
+@if "%PAUSE%"=="1" pause
+call scons.bat source dist launcher %ARGS%
+@if not "%ERRORLEVEL%"=="0" goto onerror
+
+@if "%CLIENT%"=="0" goto skip_client
+@if "%PAUSE%"=="1" pause
 cd jptools
 call buildControllerClient.cmd
+@if not "%ERRORLEVEL%"=="0" goto onerror
 cd ..
+:skip_client
+
+exit /b 0
+
+:onerror
+echo nvdajp build error %ERRORLEVEL%
+@if "%PAUSE%"=="1" pause
+exit /b -1
