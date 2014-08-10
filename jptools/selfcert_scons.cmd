1f7cbf222c690838fc3301a73424c1f964405180
Takuya Nishimoto <nishimotz@gmail.com>
2014-08-10 21:39:57 +0900

    updated selfcert build scripts

--- jptools/selfcert_scons.cmd
+++ jptools/selfcert_scons.cmd
@@ -1,2 +1,2 @@
-SET VERSION=2014.2jp
+SET VERSION=2014.3jp-beta-140811
 scons source user_docs launcher certFile=selfsigned.pfx publisher=nvdajp release=1 version=%VERSION%
--- jptools/signed_output_pack.cmd
+++ jptools/signed_output_pack.cmd
@@ -1,4 +1,4 @@
-SET VERSION=2014.2jp
-SET REV=140524
-7z a nvda-%VERSION%-%REV%-launcher-unsigned.zip output\nvda_%VERSION%.exe
-copy nvda-%VERSION%-%REV%-launcher-unsigned.zip c:\Users\nishimotz\Dropbox\Public
+SET VERSION=2014.3jp-beta-140811
+SET ZIPREV=2014.3jp-140810
+7z a nvda-%ZIPREV%-launcher-unsigned.zip output\nvda_%VERSION%.exe
+copy nvda-%ZIPREV%-launcher-unsigned.zip c:\Users\nishimotz\Dropbox\Public
