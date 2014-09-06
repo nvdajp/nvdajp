af2c44915b30080ada1812f63cb6518a37ad2ad0
Takuya Nishimoto <nishimotz@gmail.com>
2014-09-07 07:32:22 +0900

    release build script and Japanese document updates

--- jptools/selfcert_pack_dist.cmd
+++ jptools/selfcert_pack_dist.cmd
@@ -1,3 +1,3 @@
-SET ZIPREV=2014.3jp-140811
+SET ZIPREV=2014.3jp-140907
 7z a nvda-%ZIPREV%-dist-self.zip dist\*.exe
 copy nvda-%ZIPREV%-dist-self.zip c:\users\nishimotz\dropbox\public\
--- jptools/selfcert_scons.cmd
+++ jptools/selfcert_scons.cmd
@@ -1,2 +1,2 @@
-SET VERSION=2014.3jp-beta-140811
+SET VERSION=2014.3jp
 scons source user_docs launcher certFile=selfsigned.pfx publisher=nvdajp release=1 version=%VERSION%
