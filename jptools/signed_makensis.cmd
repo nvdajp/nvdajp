bfbe236016fab28deb06498d8ade07185fbf4c6b
Takuya Nishimoto <nishimotz@gmail.com>
2014-09-07 09:51:28 +0900

    build script

--- jptools/signed_makensis.cmd
+++ jptools/signed_makensis.cmd
@@ -1,3 +1,3 @@
-SET VERSION=2014.3jp-beta-140811
+SET VERSION=2014.3jp
 del output\nvda_%VERSION%.exe
 miscDeps\tools\NSIS\makensis.exe /V2 /DVERSION=%VERSION% /DPUBLISHER="nvdajp" /DCOPYRIGHT="Copyright (C) 2006-2014 NVDA Contributors" /DNVDADistDir=C:\work\nvda\nvdajp\dist /DLAUNCHEREXE=C:\work\nvda\nvdajp\output\nvda_%VERSION%.exe launcher\nvdaLauncher.nsi
--- jptools/signed_output_pack.cmd
+++ jptools/signed_output_pack.cmd
@@ -1,4 +1,4 @@
-SET VERSION=2014.3jp-beta-140811
-SET ZIPREV=2014.3jp-140810
+SET VERSION=2014.3jp
+SET ZIPREV=2014.3jp-140907
 7z a nvda-%ZIPREV%-launcher-unsigned.zip output\nvda_%VERSION%.exe
 copy nvda-%ZIPREV%-launcher-unsigned.zip c:\Users\nishimotz\Dropbox\Public
