719a40550933218328a07de22dfeff67d3afa92b
Takuya Nishimoto <nishimotz@gmail.com>
2014-11-24 15:59:41 +0900

    work around ti34542

--- jptools/kcCertBuild.cmd
+++ jptools/kcCertBuild.cmd
@@ -1,7 +1,7 @@
 set BUILDTYPE=jpbeta
 for /F "usebackq" %%t in (`python -c "from datetime import datetime as dt; print dt.now().strftime('%%y%%m%%d')"`) do set NOWDATE=%%t
 @rem set VERSION=%BUILDTYPE%%NOWDATE%
-set VERSION=2014.4jp-beta-141121
+set VERSION=2014.4jp-beta-141124
 set PUBLISHER=nvdajp
 set PFX=..\..\kc\pfx\knowlec-key141016.pfx
 set PWFILE=..\..\kc\pfx\knowlec-key-pass.txt
