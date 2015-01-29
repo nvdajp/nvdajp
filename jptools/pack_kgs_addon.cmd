eaaf82df2b6cffc62bff7851d214b15c9c67bace
Takuya Nishimoto <nishimotz@gmail.com>
2015-01-29 15:33:25 +0900

    ti34739 kgs driver should work in launcher version

--- jptools/pack_kgs_addon.cmd
+++ jptools/pack_kgs_addon.cmd
@@ -5,7 +5,7 @@ set DIRECTBM=c:\kgs\dev\DirectBM.dll
 cd ..\source
 copy /Y %DIRECTBM% source\brailleDisplayDrivers
 copy ..\jptools\kgs_manifest.ini manifest.ini
-7z a ..\jptools\_kgs.zip manifest.ini brailleDisplayDrivers\kgs.py brailleDisplayDrivers\kgsbn46.py brailleDisplayDrivers\DirectBM.dll
+7z a ..\jptools\_kgs.zip manifest.ini brailleDisplayDrivers\kgs.py brailleDisplayDrivers\DirectBM.dll
 del manifest.ini
 cd ..\jptools
-move _kgs.zip kgsbraille-1.9.0.nvda-addon
+move _kgs.zip kgsbraille-1.9.1.nvda-addon
