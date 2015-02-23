0953fa4eb3d325b34637837fd741111e16a94bef
Takuya Nishimoto <nishimotz@gmail.com>
2015-02-23 13:33:43 +0900

    fixed findBackupFiles

--- jptools/findBackupFiles.cmd
+++ jptools/findBackupFiles.cmd
@@ -1,7 +1,5 @@
-set find=C:\Applications\gnupack\app\cygwin\cygwin\bin\find
-%find% . -name "*~" -type f -delete
+set find="c:\Program Files\Git\bin\find.exe"
 %find% . -name "*~" -type f
-
-%find% . -name "*.pyc" -type f -delete
 %find% . -name "*.pyc" -type f
+
 
