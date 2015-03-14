f197cdb2ba73355ce7acfdc73277acea477f1341
Takuya Nishimoto <nishimotz@gmail.com>
2015-03-14 21:44:19 +0900

    jpbeta non cert build

--- jptools/findBackupFiles.cmd
+++ jptools/findBackupFiles.cmd
@@ -1,5 +1,5 @@
 set find="c:\Program Files\Git\bin\find.exe"
 %find% . -name "*~" -type f
-%find% . -name "*.pyc" -type f
+@rem %find% . -name "*.pyc" -type f
 
 
---
+++ jptools/nonCertAllBuild.cmd
@@ -0,0 +1,4 @@
+call scons.bat -c
+call jptools\setup-vc2013.cmd
+call jptools\setupMiscDepsJp.cmd
+call jptools\build.cmd
