5aef886bb5369d3c26fa8cab1add913b24927a2d
Takuya Nishimoto <nishimotz@gmail.com>
2015-05-24 19:25:33 +0900

    updated build tools

--- jptools/nonCertAllBuild.cmd
+++ jptools/nonCertAllBuild.cmd
@@ -1,4 +1,14 @@
+@rem test nmake and check errorlevel
+cl
+if "%ERRORLEVEL%" neq "9009" goto :done
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat" goto x64
+call "C:\Program Files\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
+goto done
+:x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
+:done
+SET CL=/arch:IA32 /D "_USING_V110_SDK71_"
 call scons.bat -c
-call jptools\setup-vc2013.cmd
 call jptools\setupMiscDepsJp.cmd
 call jptools\build.cmd
