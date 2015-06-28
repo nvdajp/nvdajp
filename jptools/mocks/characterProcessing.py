8a27796e005385ed88511dc21a524c37b4b37a98
Takuya Nishimoto <nishimotz@gmail.com>
2015-06-28 22:14:13 +0900

    ti35244: jpDicTest should work

---
+++ jptools/mocks/characterProcessing.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals, print_function
+import codecs
+
+_entries = {}
+_readings = {}
+
+def setup():
+	global _entries, _readings
+	fname = r"..\source\locale\ja\characters.dic"
+	linenum = 0
+	with codecs.open(fname,"r","utf_8_sig",errors="replace") as f:
+		for line in f:
+			if line.isspace() or line.startswith('#'):
+				continue
+			line=line.rstrip('\r\n')
+			temp=line.split("\t")
+			if len(temp) > 1:
+				key=temp.pop(0)
+				code=temp.pop(0)
+				rd=temp.pop(0)
+				if rd.startswith('[') and rd.endswith(']'):
+					_readings[key] = rd[1:-1]
+					_entries[key] = temp[0]
+				else:
+					print("%d: can't parse '%s'" % (linenum, line))
+			linenum += 1
+
+setup()
+
+def processSpeechSymbol(lang, s):
+	assert lang == 'ja'
+	return _entries.get(s, s)
+
+def getCharacterReading(lang, s):
+	assert lang == 'ja'
+	return _readings.get(s, s)
