7b481b4105a991c997edf5eb8ac45eb1dde74113
Takuya Nishimoto <nishimotz@gmail.com>
2015-06-30 18:55:26 +0900

    ti35296 tool for maintain symbols.dic

---
+++ jpchar/annotateSymbolsDic.py
@@ -0,0 +1,27 @@
+# coding: UTF-8
+# annotateSymbolsDic.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2015 Takuya Nishimoto (NVDA Japanese Team)
+#
+# annotate Unicode numbers as comments to symbols
+from __future__ import print_function
+import pprint
+
+import _checkCharDesc as cd
+
+def convert(sy_file, outfile):
+	sy, src = cd.read_symbol_file(sy_file, returnSource=True)
+	with open(outfile, 'w') as of:
+		for line in src:
+			of.write(line.encode('utf-8') + "\n")
+
+convert(
+	r"..\..\srt\ja\symbols-newRevisions\11146\symbols.dic",
+	'_en.dic'
+)
+
+convert(
+	r"..\source\locale\ja\symbols.dic",
+	'_ja.dic'
+)
+
