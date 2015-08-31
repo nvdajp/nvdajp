bf3e2d48b9bcc3c8cf168c38318bf78a5b59a9e6
Takuya Nishimoto <nishimotz@gmail.com>
2015-08-31 20:31:07 +0900

    2015.4jp-beta

--- jptools/nonCertBuild.cmd
+++ jptools/nonCertBuild.cmd
@@ -3,7 +3,7 @@ set DIRECTBM=c:\kgs\dev\DirectBM.dll
 copy /Y %DIRECTBM% source\brailleDisplayDrivers
 @rem end newdirectbm
 
-set VERSION=2015.3jp
+set VERSION=2015.4jp
 for /F "usebackq" %%t in (`python -c "from datetime import datetime as dt; print dt.now().strftime('%%y%%m%%d')"`) do set NOWDATE=%%t
 set VERSION=%VERSION%-beta-%NOWDATE%
 set PUBLISHER=nvdajp
