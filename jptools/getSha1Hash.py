25ce17efe1ee7520477fbb4931680624cfe7bc37
Takuya Nishimoto <nishimotz@gmail.com>
2015-09-17 11:44:46 +0900

    update DirectBM and add getSha1Hash

---
+++ jptools/getSha1Hash.py
@@ -0,0 +1,19 @@
+# @param fileHash: The SHA-1 hash of the file as a hex string.
+# @type fileHash: basestring
+
+from __future__ import unicode_literals, print_function
+import hashlib
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('fileName')
+args = ap.parse_args()
+
+fd = open(args.fileName, 'rb')
+data = fd.read()
+fd.close()
+
+hasher = hashlib.sha1()
+hasher.update(data)
+print(args.fileName)
+print("launcherHash: %s" % hasher.hexdigest())
--- jptools/pack_kgs_addon.cmd
+++ jptools/pack_kgs_addon.cmd
@@ -1,9 +1,7 @@
 @rem usage:
 @rem > cd jptools
 @rem > pack_kgs_addon.cmd
-set DIRECTBM=c:\kgs\dev\DirectBM.dll
 cd ..\source
-copy /Y %DIRECTBM% brailleDisplayDrivers
 copy ..\jptools\kgs_manifest.ini manifest.ini
 7z a ..\jptools\_kgs.zip manifest.ini brailleDisplayDrivers\kgs.py brailleDisplayDrivers\DirectBM.dll
 del manifest.ini
