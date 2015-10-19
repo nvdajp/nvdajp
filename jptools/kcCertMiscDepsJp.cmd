3509edd290f38cf0bfcee8535db94d81b67ae975
Takuya Nishimoto <nishimotz@gmail.com>
2015-10-19 13:42:23 +0900

    updated knowlec cert

--- jptools/kcCertMiscDepsJp.cmd
+++ jptools/kcCertMiscDepsJp.cmd
@@ -1,14 +1,13 @@
-set PFX=..\..\kc\pfx\knowlec-key141016.pfx
+set PFX=..\..\kc\pfx\knowlec-key151019.pfx
 set PWFILE=..\..\kc\pfx\knowlec-key-pass.txt
 for /F "delims=" %%s in ('type %PWFILE%') do set PASSWORD=%%s
 set TIMESERVER=http://timestamp.comodoca.com/authenticode
-set DESC=nvdajp
 set FILE1=source\synthDrivers\jtalk\libmecab.dll
 set FILE2=source\synthDrivers\jtalk\libopenjtalk.dll
-@rem set FILE3=source\brailleDisplayDrivers\DirectBM.dll
+signtool sign /f %PFX% /p %PASSWORD% /t %TIMESERVER% %FILE1%
+signtool verify /pa %FILE1%
+timeout /T 5 /NOBREAK
+signtool sign /f %PFX% /p %PASSWORD% /t %TIMESERVER% %FILE2%
+signtool verify /pa %FILE2%
+timeout /T 5 /NOBREAK
 
-signtool sign /f %PFX% /p %PASSWORD% /t %TIMESERVER% /d %DESC% %FILE1%
-timeout /T 3 /NOBREAK
-signtool sign /f %PFX% /p %PASSWORD% /t %TIMESERVER% /d %DESC% %FILE2%
-timeout /T 3 /NOBREAK
-@rem signtool sign /f %PFX% /p %PASSWORD% /t %TIMESERVER% /d %DESC% %FILE3%
