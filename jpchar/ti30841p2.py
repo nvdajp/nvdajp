365badd87d35960c1cc075d62f3cd2745ca38ac8
Takuya Nishimoto <nishimotz@gmail.com>
2016-06-19 17:07:18 +0900

    nvdajp/nvdajp#7 ti30841

---
+++ jpchar/ti30841p2.py
@@ -0,0 +1,25 @@
+# coding: UTF-8
+# ti30841p2
+#
+# input ../source/locale/ja/characters.dic
+# 🌀	1f300	[グルグル]	タイフウ グルグルノ エモジ
+#
+# output (stdout)
+# 🌀	グルグル	none
+
+from __future__ import print_function
+
+import codecs
+
+with codecs.open('../source/locale/ja/characters.dic', 'r', 'utf-8') as file:
+	lines = file.readlines()
+
+for line in lines:
+	line = line.rstrip()
+	if len(line) == 0 or line[0] == '#':
+		print('')
+		continue
+	fields = line.split('\t')
+	if len(fields[1]) == 5:
+		s = u"%s\t%s\tnone" % (fields[0], fields[2].replace('[', '').replace(']', ''))
+		print(s.encode('utf-8'))
