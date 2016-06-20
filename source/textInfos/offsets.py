f01734c7059d2dc284186c7b0c08085f27aedb7b
Takuya Nishimoto <nishimotz@gmail.com>
2016-06-20 22:19:01 +0900

    nvdajp/nvdajp#7 ti30841 work around NotImplementedError

--- source/textInfos/offsets.py
+++ source/textInfos/offsets.py
@@ -223,23 +223,25 @@ class OffsetsTextInfo(textInfos.TextInfo):
 
 	def _getCharacterOffsets(self,offset):
 		#return [offset,offset+1]
-		if not hasattr(self,'_getStoryText') or not hasattr(self,'_getStoryLength'):
+		try:
+			text = self._getStoryText()
+			textlen = self._getStoryLength() - 1
+			log.debug(repr([len(text), textlen, offset]))
+			if textlen <= offset:
+				return [offset,offset+1]
+			uc = ord(text[offset])
+			if (0xd800 <= uc <= 0xdbff) and offset+1 < textlen:
+				r = [offset,offset+2]
+				uc2 = ord(text[offset+1])
+				uc4 = (uc - 0xd800) * 0x800 + (uc2 - 0xdc00)
+				log.debug(repr([text, r, ("%04x %04x %05x" % (uc, uc2, uc4))]))
+			else:
+				r = [offset,offset+1]
+				log.debug(repr([text, r, ("%04x" % uc)]))
+			return r
+		except NotImplementedError:
 			return [offset,offset+1]
-		text = self._getStoryText()
-		textlen = self._getStoryLength() - 1
-		log.debug(repr([len(text), textlen, offset]))
-		if textlen <= offset:
-			return [offset,offset+1]
-		uc = ord(text[offset])
-		if (0xd800 <= uc <= 0xdbff) and offset+1 < textlen:
-			r = [offset,offset+2]
-			uc2 = ord(text[offset+1])
-			uc4 = (uc - 0xd800) * 0x800 + (uc2 - 0xdc00)
-			log.debug(repr([text, r, ("%04x %04x %05x" % (uc, uc2, uc4))]))
-		else:
-			r = [offset,offset+1]
-			log.debug(repr([text, r, ("%04x" % uc)]))
-		return r
+			
 
 	def _getWordOffsets(self,offset):
 		lineStart,lineEnd=self._getLineOffsets(offset)
