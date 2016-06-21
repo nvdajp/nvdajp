031d5e0f06aca4dce9b8b7181272a853941b76c9
Takuya Nishimoto <nishimotz@gmail.com>
2016-06-21 19:41:19 +0900

    nvdajp/nvdajp#7 ti30841

--- source/textInfos/offsets.py
+++ source/textInfos/offsets.py
@@ -222,25 +222,11 @@ class OffsetsTextInfo(textInfos.TextInfo):
 		return formatField,(startOffset,endOffset)
 
 	def _getCharacterOffsets(self,offset):
-		#return [offset,offset+1]
 		try:
 			text = self._getStoryText()
 			textlen = self._getStoryLength() - 1
-			#log.debug(repr([len(text), textlen, offset]))
-			#if textlen <= offset:
-			#	return [offset,offset+1]
 			if offset+1 < textlen and unicodedata.category(text[offset]) == "Cs":
 				return [offset,offset+2]
-			#uc = ord(text[offset])
-			#if (0xd800 <= uc <= 0xdbff) and offset+1 < textlen:
-			#	r = [offset,offset+2]
-			#	uc2 = ord(text[offset+1])
-			#	uc4 = (uc - 0xd800) * 0x800 + (uc2 - 0xdc00)
-			#	log.debug(repr([text, r, ("%04x %04x %05x" % (uc, uc2, uc4))]))
-			#else:
-			#	r = [offset,offset+1]
-			#	log.debug(repr([text, r, ("%04x" % uc)]))
-			#return r
 		except NotImplementedError:
 			pass
 		return [offset,offset+1]
