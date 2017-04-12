9f77fa89baaf55cb1db1e0c29530bae5bdb1bed1
Takuya Nishimoto <nishimotz@gmail.com>
2017-04-12 15:08:21 +0900

    enable unittest with nvdajp

--- jptools/nonCertBuild.cmd
+++ jptools/nonCertBuild.cmd
@@ -1,1 +1,4 @@
-@scons source user_docs launcher publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
+@set BUILDOPTIONS=publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
+@scons source %BUILDOPTIONS%
+@scons tests %BUILDOPTIONS%
+@scons user_docs launcher %BUILDOPTIONS%
