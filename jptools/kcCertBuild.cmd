7d299e323905c4543dcc338b291c33679340d337
Takuya Nishimoto <nishimotz@gmail.com>
2017-04-12 15:08:21 +0900

    enable unittest with nvdajp

--- jptools/kcCertBuild.cmd
+++ jptools/kcCertBuild.cmd
@@ -1,1 +1,4 @@
-@scons source user_docs launcher certFile=%PFX% certPassword=%PASSWORD% certTimestampServer=%TIMESERVER% publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
+@set BUILDOPTIONS=certFile=%PFX% certPassword=%PASSWORD% certTimestampServer=%TIMESERVER% publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
+@scons source %BUILDOPTIONS%
+@scons tests %BUILDOPTIONS%
+@scons user_docs launcher %BUILDOPTIONS%
