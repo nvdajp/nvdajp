4a9b955b37c35f38c879be03df07e284d4cf99a0
Takuya Nishimoto <nishimotz@gmail.com>
2017-04-12 16:47:08 +0900

    reverted to @scons source publisher=nvdajp release=1 version=2017.1jp-beta-170412 updateVersionType=nvdajpbeta  --silent

--- jptools/kcCertBuild.cmd
+++ jptools/kcCertBuild.cmd
@@ -1,4 +1,1 @@
-@set BUILDOPTIONS=certFile=%PFX% certPassword=%PASSWORD% certTimestampServer=%TIMESERVER% publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
-@scons source %BUILDOPTIONS%
-@scons tests %BUILDOPTIONS%
-@scons user_docs launcher %BUILDOPTIONS%
+@scons source user_docs launcher certFile=%PFX% certPassword=%PASSWORD% certTimestampServer=%TIMESERVER% publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
--- jptools/nonCertBuild.cmd
+++ jptools/nonCertBuild.cmd
@@ -1,4 +1,1 @@
-@set BUILDOPTIONS=publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
-@scons source %BUILDOPTIONS%
-@scons tests %BUILDOPTIONS%
-@scons user_docs launcher %BUILDOPTIONS%
+@scons source user_docs launcher publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
