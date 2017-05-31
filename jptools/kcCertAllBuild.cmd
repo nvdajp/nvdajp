de513c7f2f0b1e17ff02329a177f03ee25c0849f
Takuya Nishimoto <nishimotz@gmail.com>
2017-05-31 12:33:54 +0900

    prepare for 2017.3jp

--- jptools/kcCertAllBuild.cmd
+++ jptools/kcCertAllBuild.cmd
@@ -1,12 +1,12 @@
 set SCONSOPTIONS=%* --silent
 
-set VERSION=2017.2jp
+set VERSION=2017.3jp
 set UPDATEVERSIONTYPE=nvdajp
 
 for /F "usebackq" %%t in (`python -c "from datetime import datetime as dt; print dt.now().strftime('%%y%%m%%d')"`) do set NOWDATE=%%t
-@rem set VERSION=%VERSION%-beta
-@rem set VERSION=%VERSION%-%NOWDATE%
-@rem set UPDATEVERSIONTYPE=%UPDATEVERSIONTYPE%beta
+set VERSION=%VERSION%-beta
+set VERSION=%VERSION%-%NOWDATE%
+set UPDATEVERSIONTYPE=%UPDATEVERSIONTYPE%beta
 
 set PUBLISHER=nvdajp
 set PFX=jptools\secret\knowlec-key161005.pfx
