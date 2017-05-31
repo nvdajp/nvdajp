a5e08c3fc7b1b35bbf7770b16492a3f76927c53e
Takuya Nishimoto <nishimotz@gmail.com>
2017-05-31 12:33:54 +0900

    prepare for 2017.3jp

--- jptools/nonCertAllBuild.cmd
+++ jptools/nonCertAllBuild.cmd
@@ -1,6 +1,6 @@
 set SCONSOPTIONS=%* --silent
 
-set VERSION=2017.2jp
+set VERSION=2017.3jp
 set UPDATEVERSIONTYPE=nvdajp
 
 for /F "usebackq" %%t in (`python -c "from datetime import datetime as dt; print dt.now().strftime('%%y%%m%%d')"`) do set NOWDATE=%%t
