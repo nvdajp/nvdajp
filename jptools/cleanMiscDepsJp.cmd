08fe570db51dd56626e2c207579d2a6268c9a324
Takuya Nishimoto <nishimotz@gmail.com>
2017-08-02 17:04:26 +0900

    updated cleanup tools

--- jptools/cleanMiscDepsJp.cmd
+++ jptools/cleanMiscDepsJp.cmd
@@ -1,11 +1,12 @@
-cd ..\miscDepsJp
-cd include\jtalk\libopenjtalk\mecab
-nmake /f Makefile.mak clean
-cd ..\..\..\..
-cd source\synthDrivers\jtalk
-del /Q *.pyc
-cd ..\..\..
-cd ..\jptools
-
-
-
+@rem (usage)
+@rem > jptools\cleanMiscDepsJp.cmd
+del /s /q miscDepsJp\jptools\__h1output.txt
+del /s /q miscDepsJp\jptools\__h2output.txt
+del /s /q miscDepsJp\jptools\__test_log.txt
+del /s /q miscDepsJp\jptools\harness.pyc
+del /s /q miscDepsJp\jptools\jpBrailleRunner.pyc
+del /s /q miscDepsJp\jptools\jtalkPredicTest.pyc
+del /s /q miscDepsJp\jptools\jtalkRunner.pyc
+del /s /q miscDepsJp\jptools\jtusr.dic
+del /s /q miscDepsJp\jptools\mecabRunner.pyc
+del /s /q miscDepsJp\jptools\nabccHarness.pyc
