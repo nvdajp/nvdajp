65da360fb5f6b4b9ff112ba0b953d0ef96b38dce
Takuya Nishimoto <nishimotz@gmail.com>
2017-09-21 18:17:33 +0900

    nvdajp/nvdajp#56

--- jptools/nonCertAllBuild.cmd
+++ jptools/nonCertAllBuild.cmd
@@ -9,10 +9,6 @@ set VERSION=%VERSION%-%NOWDATE%
 set UPDATEVERSIONTYPE=%UPDATEVERSIONTYPE%beta
 
 set PUBLISHER=nvdajp
-@rem set PFX=jptools\secret\knowlec-key161005.pfx
-@rem set PWFILE=jptools\secret\knowlec-key-pass.txt
-@rem @for /F "delims=" %%s in ('type %PWFILE%') do @set PASSWORD=%%s
-@rem set TIMESERVER=http://timestamp.comodoca.com/authenticode
 
 @rem test nmake and check errorlevel
 cl
@@ -25,9 +21,6 @@ goto done
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 :done
 SET CL=/arch:IA32 /D "_USING_V110_SDK71_"
-
-@rem set VERIFYLOG=output\nvda_%VERSION%_verify.log
-@rem del /Q %VERIFYLOG%
 
 cd miscDepsJp\jptools
 call clean.cmd
@@ -43,30 +36,7 @@ python scons.py source publisher=%PUBLISHER% release=1 version=%VERSION% updateV
 python scons.py tests publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
 @if not "%ERRORLEVEL%"=="0" goto onerror
 
-@rem set FILE1=source\synthDrivers\jtalk\libmecab.dll
-@rem @signtool sign /f %PFX% /p %PASSWORD% /t %TIMESERVER% %FILE1%
-@rem signtool verify /pa %FILE1% >> %VERIFYLOG%
-@rem @if not "%ERRORLEVEL%"=="0" goto onerror
-@rem timeout /T 5 /NOBREAK
-
-@rem set FILE2=source\synthDrivers\jtalk\libopenjtalk.dll
-@rem @signtool sign /f %PFX% /p %PASSWORD% /t %TIMESERVER% %FILE2%
-@rem signtool verify /pa %FILE2% >> %VERIFYLOG%
-@rem @if not "%ERRORLEVEL%"=="0" goto onerror
-@rem timeout /T 5 /NOBREAK
-
 call jptools\nonCertBuild.cmd
-
-@rem signtool verify /pa dist\lib\*.dll >> %VERIFYLOG%
-@rem @if not "%ERRORLEVEL%"=="0" goto onerror
-@rem signtool verify /pa dist\lib64\*.dll >> %VERIFYLOG%
-@rem @if not "%ERRORLEVEL%"=="0" goto onerror
-@rem signtool verify /pa dist\*.exe >> %VERIFYLOG%
-@rem @if not "%ERRORLEVEL%"=="0" goto onerror
-@rem signtool verify /pa output\nvda_%VERSION%.exe >> %VERIFYLOG%
-@rem @if not "%ERRORLEVEL%"=="0" goto onerror
-
-@rem copy output\nvda_%VERSION%.exe %USERPROFILE%\Dropbox\Public
 
 cd jptools
 call buildControllerClient.cmd
