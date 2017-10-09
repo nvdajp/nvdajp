4ae18a227f975eac7d6d258a64e88cbc3d35dda5
Takuya Nishimoto <nishimotz@gmail.com>
2017-10-09 18:50:23 +0900

    use Visual Studio 2017 Community if available

--- jptools/selfcert_clean.cmd
+++ jptools/selfcert_clean.cmd
@@ -1,5 +1,5 @@
 del /Q /F selfsigned.* .sconsign.dblite
-call ..\miscDepsJp\include\python-jtalk\vcsetup.cmd
+call miscDepsJp\include\python-jtalk\vcsetup.cmd
 color
 certutil -delstore root selfsigned.spc
 scons -c
