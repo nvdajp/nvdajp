c5d0e7d2feb6fc4e0d0ccabec4e4285ab7f181f6
Takuya Nishimoto <nishimotz@gmail.com>
2017-10-11 23:19:33 +0900

    should not overwrite appveyor.yml

--- jptools/setupMiscDepsJp.cmd
+++ jptools/setupMiscDepsJp.cmd
@@ -17,7 +17,7 @@ cd ..\..\..\..
 cd source\synthDrivers
 rmdir /S /Q espeak-data
 cd ..\..
-7z a -xr!.bzr* -xr!.git* -xr!_temp -xr!_pack*.cmd -xr!_push*.cmd -xr!*.obj -xr!*~ -xr!*.rst -xr!*.html -xr!include\jtalk\* -xr!include\python-jtalk\* -xr!jptools -xr!include\espeak\* -xr!include\AcrobatAccess\* -xr!include\ia2\* -xr!include\libMinHook\* -xr!source\brlapi.pyd -xr!source\configobj.py -xr!source\validate.py -xr!txt2tags.py -xr!installer\UAC.dll -xr!installer\waves\* -xr!tools\* -xr!uninstaller\* -xr!launcher\* -xr!source\images\nvda.ico -xr!source\waves\* ..\nvdajp-miscdep.7z *
+7z a -xr!.bzr* -xr!.git* -xr!_temp -xr!_pack*.cmd -xr!_push*.cmd -xr!*.obj -xr!*~ -xr!*.rst -xr!*.html -xr!include\jtalk\* -xr!include\python-jtalk\* -xr!jptools -xr!include\espeak\* -xr!include\AcrobatAccess\* -xr!include\ia2\* -xr!include\libMinHook\* -xr!source\brlapi.pyd -xr!source\configobj.py -xr!source\validate.py -xr!txt2tags.py -xr!installer\UAC.dll -xr!installer\waves\* -xr!tools\* -xr!uninstaller\* -xr!launcher\* -xr!source\images\nvda.ico -xr!source\waves\* -xr!appveyor.yml ..\nvdajp-miscdep.7z *
 cd ..
 7z x -y nvdajp-miscdep.7z
 del /Q nvdajp-miscdep.7z
