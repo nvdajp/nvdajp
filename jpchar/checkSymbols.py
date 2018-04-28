d8e438f4f033c6d4c81fda3de070d63fe6087e2d
Takuya Nishimoto <nishimotz@gmail.com>
2018-04-28 16:01:21 +0900

    nvdajp/nvdajp#67 nvdajp/nvdajp#75

--- jpchar/checkSymbols.py
+++ jpchar/checkSymbols.py
@@ -6,10 +6,10 @@
 # > cd jpchar
 # > python checkSymbols.py
 
-import sys
-sys.path.append(r'..\source\synthDrivers\jtalk')
+#import sys
+#sys.path.append(r'..\source\synthDrivers\jtalk')
 from _checkCharDesc import *
 sy = read_symbol_file(SY_FILE)
 #ch = read_chardesc_file(CH_FILE)
-ch = read_characters_file(CS_FILE)
+ch = read_characters_file(CS_FILE, use_both=True)
 print_different(sy, ch, skip_included=True)
