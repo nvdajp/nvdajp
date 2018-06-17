c07424f45485ef9f37a8777bd4087c1413541118
Takuya Nishimoto <nishimotz@gmail.com>
2018-06-17 14:16:27 +0900

    nvdajp/nvdajp#89

--- jptools/kcCertAllBuild.cmd
+++ jptools/kcCertAllBuild.cmd
@@ -1,12 +1,12 @@
 set SCONSOPTIONS=%* --silent
 
-set VERSION=2018.3jp
+set VERSION=2018.2.1jp
 set UPDATEVERSIONTYPE=nvdajp
 
 for /F "usebackq" %%t in (`python -c "from datetime import datetime as dt; print dt.now().strftime('%%y%%m%%d')+chr(dt.now().hour+97)"`) do set NOWDATE=%%t
-set VERSION=%VERSION%-beta
-set VERSION=%VERSION%-%NOWDATE%
-set UPDATEVERSIONTYPE=%UPDATEVERSIONTYPE%beta
+@rem set VERSION=%VERSION%-beta
+@rem set VERSION=%VERSION%-%NOWDATE%
+@rem set UPDATEVERSIONTYPE=%UPDATEVERSIONTYPE%beta
 echo %UPDATEVERSIONTYPE% %VERSION%
 
 set PUBLISHER=nvdajp
