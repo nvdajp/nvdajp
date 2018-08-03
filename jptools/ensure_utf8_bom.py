9d6a9aeac1c02f743ac8ca5b9523e2d01b24c8fe
Takuya Nishimoto <nishimotz@gmail.com>
2018-08-03 17:19:00 +0900

    nvdajp/nvdajp#93 (jpbeta)

---
+++ jptools/ensure_utf8_bom.py
@@ -0,0 +1,21 @@
+# coding: UTF-8
+# Usage:
+# > python jptools\ensure_utf8_bom.py include\espeak\src\libespeak-ng\tr_languages.c
+
+from __future__ import unicode_literals, print_function
+import sys
+import codecs
+
+if __name__ == '__main__':
+    fileName = sys.argv[1]
+    print(fileName)
+    if codecs.open(fileName,"r","utf_8",errors="replace").readline()[0] == '\ufeff':
+        print('with bom')
+    else:
+        print('without bom')
+        with codecs.open(fileName,"r","utf_8",errors="replace") as f:
+            data = f.read()
+        with codecs.open(fileName,"w","utf_8_sig",errors="replace") as f:
+            f.write(data)
+        print('converted to utf8 with bom')
+            
