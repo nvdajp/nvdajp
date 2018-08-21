d7eab0ddf5dcc6a6cb81709a4714028b4e15324c
Takuya Nishimoto <nishimotz@gmail.com>
2018-08-21 23:06:42 +0900

    revert jptools

---
+++ jptools/kcCertBuild.cmd
@@ -0,0 +1,1 @@
+@scons source user_docs launcher certFile=%PFX% certPassword=%PASSWORD% certTimestampServer=%TIMESERVER% publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
