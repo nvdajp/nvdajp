5d5576e78727367f005360e02e8f80b8d75051af
Takuya Nishimoto <nishimotz@gmail.com>
2018-08-21 21:57:35 +0900

    refactor jptools

--- jptools/nonCertAllBuild.cmd
+++ jptools/nonCertAllBuild.cmd
@@ -1,16 +1,5 @@
 set SCONSOPTIONS=%* --silent
 
-rem set VERSION=2018.3jp
-rem set UPDATEVERSIONTYPE=nvdajp
-rem 
-rem @for /F "usebackq" %%t in (`jptools\nowdate.cmd`) do set NOWDATE=%%t
-rem set VERSION=%VERSION%-beta
-rem set VERSION=%VERSION%-%NOWDATE%
-rem set VERSION=%VERSION%-noncert
-rem set UPDATEVERSIONTYPE=%UPDATEVERSIONTYPE%beta
-rem 
-rem set PUBLISHER=nvdajp
-
 call miscDepsJp\include\python-jtalk\vcsetup.cmd
 cd /d %~dp0
 cd ..
@@ -21,7 +10,6 @@ call build-and-test.cmd
 @if not "%ERRORLEVEL%"=="0" goto onerror
 cd ..\..
 
-@rem python scons.py -c
 call jptools\setupMiscDepsJp.cmd
 
 python jptools\ensure_utf8_bom.py include\espeak\src\libespeak-ng\tr_languages.c
