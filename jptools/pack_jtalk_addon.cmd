161c9091846d5f7a815baa749152e4f2043fcb7f
Takuya Nishimoto <nishimotz@gmail.com>
2018-08-26 21:10:56 +0900

    appveyor build of nvdajp-jtalk-180826.nvda-addon

--- jptools/pack_jtalk_addon.cmd
+++ jptools/pack_jtalk_addon.cmd
@@ -7,4 +7,4 @@ copy ..\jptools\jtalk_manifest.ini manifest.ini
 7z a ..\jptools\nvdajp-jtalk.zip manifest.ini synthDrivers\nvdajp*.py synthDrivers\jtalk\*
 del manifest.ini
 cd ..\jptools
-move nvdajp-jtalk.zip nvdajp-jtalk-130521.nvda-addon
+move nvdajp-jtalk.zip nvdajp-jtalk-180826.nvda-addon
