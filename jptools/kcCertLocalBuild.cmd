0943e84340f0336bed12ad595fbeabb49880b973
Takuya Nishimoto <nishimotz@gmail.com>
2018-10-24 22:12:51 +0900

    update espeak

--- jptools/kcCertLocalBuild.cmd
+++ jptools/kcCertLocalBuild.cmd
@@ -1,10 +1,9 @@
 set RELEASE=1
-set VERSION=2018.4jp
+set VERSION=jpalpha
 set UPDATEVERSIONTYPE=nvdajp
 for /F "usebackq" %%t in (`jptools\nowdate.cmd`) do set NOWDATE=%%t
-set VERSION=%VERSION%-beta
-set VERSION=%VERSION%-%NOWDATE%
-set UPDATEVERSIONTYPE=%UPDATEVERSIONTYPE%beta
+set VERSION=%VERSION%_%NOWDATE%
+set UPDATEVERSIONTYPE=%UPDATEVERSIONTYPE%alpha
 set PUBLISHER=nvdajp
 copy c:\work\kc\pfx\knowlec-key-pass-2018.txt jptools\secret
 del source\_buildVersion.py*
