43315e18be5cdffcd245023f3ba1061f7f51ea4f
Takuya Nishimoto <nishimotz@gmail.com>
2019-06-01 09:54:21 +0900

    revert nvdajp/nvdajp#198

--- source/appModuleHandler.py
+++ source/appModuleHandler.py
@@ -168,9 +168,7 @@ def fetchAppModule(processID,appName):
 	# First, check whether the module exists.
 	# We need to do this separately because even though an ImportError is raised when a module can't be found, it might also be raised for other reasons.
 	# Python 2.x can't properly handle unicode module names, so convert them.
-	modName = appName
-	if sys.version_info.major == 2 and not isinstance(modName, bytes):
-		modName = modName.encode("mbcs")
+	modName = appName.encode("mbcs")
 
 	if doesAppModuleExist(modName):
 		try:
