039321a7c0f4430fee2ed2fcbb20482ca40759df
Takuya Nishimoto <nishimotz@gmail.com>
2019-08-01 18:03:15 +0900

    #219

--- jptools/tests.cmd
+++ jptools/tests.cmd
@@ -1,8 +1,8 @@
-python scons.py tests checkPot publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
+py scons.py tests checkPot publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
 @if not "%ERRORLEVEL%"=="0" goto onerror
 
 cd jptools
-python jpDicTest.py > __jpdictest_log.txt
+py jpDicTest.py > __jpdictest_log.txt
 cd ..
 
 exit /b 0
