340ddc7249d6ff4ed9bfcc307511f07f43b957e4
Takuya Nishimoto <nishimotz@gmail.com>
2019-08-01 21:12:17 +0900

    #219 appveyor-jp-noncert

--- jptools/nonCertAllBuild.cmd
+++ jptools/nonCertAllBuild.cmd
@@ -1,12 +1,5 @@
 set SCONSOPTIONS=%* --silent
 
-@rem set UPDATEVERSIONTYPE=nvdajpalpha
-@rem 
-@rem for /F "usebackq" %%t in (`python -c "from datetime import datetime as dt; print dt.now().strftime('%%y%%m%%d')+chr(dt.now().hour+97)"`) do set NOWDATE=%%t
-@rem set VERSION=jpalpha_%NOWDATE%_noncert
-@rem 
-@rem set PUBLISHER=nvdajp
-@rem 
 call miscDepsJp\include\python-jtalk\vcsetup.cmd
 cd /d %~dp0
 cd ..
@@ -19,12 +12,7 @@ cd ..\..
 
 call jptools\setupMiscDepsJp.cmd
 
-python scons.py source user_docs launcher publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
-
-@rem cd jptools
-@rem call buildControllerClient.cmd
-@rem @if not "%ERRORLEVEL%"=="0" goto onerror
-@rem cd ..
+py scons.py source user_docs launcher publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
 
 exit /b 0
 
