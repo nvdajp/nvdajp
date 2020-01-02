f8b8270f0d446d11b884bc85f7161d8f46da3d78
Takuya Nishimoto <nishimotz@gmail.com>
2020-01-02 13:38:08 +0900

    https://github.com/nvdajp/nvdajp/issues/138

--- source/globalCommands.py
+++ source/globalCommands.py
@@ -233,16 +233,16 @@ class GlobalCommands(ScriptableObject):
 			mo = re.match('(\d{1,2}):(\d{2})', text)
 			if mo:
 				hour, minute = mo.group(1), mo.group(2)
-				if hour[0] == '0': hour = hour[1:]
-				if minute[0] == '0': minute = minute[1:]
+				if len(hour) == 2 and hour[0] == '0': hour = hour[1:]
+				if len(minute) == 2 and minute[0] == '0': minute = minute[1:]
 				# Translators: hour and minute
 				text = _('{hour}:{minute}').format(hour=hour, minute=minute)
 			else:
 				mo = re.match('([^\d]+)(\d{1,2}):(\d{2})', text)
 				if mo:
 					am_or_pm, hour, minute = mo.group(1), mo.group(2), mo.group(3)
-					if hour[0] == '0': hour = hour[1:]
-					if minute[0] == '0': minute = minute[1:]
+					if len(hour) == 2 and hour[0] == '0': hour = hour[1:]
+					if len(minute) == 2 and minute[0] == '0': minute = minute[1:]
 					# Translators: hour and minute
 					text = am_or_pm + _('{hour}:{minute}').format(hour=hour, minute=minute)
 			# nvdajp end
