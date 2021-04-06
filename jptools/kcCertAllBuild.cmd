a2635c7b4662bab21995be5695b422453a239efd
Takuya Nishimoto <nishimotz@gmail.com>
2021-04-06 19:27:58 +0900

    #290 kcCertAllBuild wip

--- jptools/kcCertAllBuild.cmd
+++ jptools/kcCertAllBuild.cmd
@@ -4,11 +4,8 @@ call miscDepsJp\include\python-jtalk\vcsetup.cmd
 cd /d %~dp0
 cd ..
 
-cd miscDepsJp\jptools
-call clean.cmd
-call build-and-test.cmd
-@if not "%ERRORLEVEL%"=="0" goto onerror
-cd ..\..
+
+
 
 call jptools\setupMiscDepsJp.cmd
 
