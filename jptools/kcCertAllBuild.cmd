91dd7a78abf8f25754ce1f84f0807f520ce4b165
Takuya Nishimoto <nishimotz@gmail.com>
2021-04-06 19:09:33 +0900

    #290 disable kcCert temporally

--- jptools/kcCertAllBuild.cmd
+++ jptools/kcCertAllBuild.cmd
@@ -1,11 +1,5 @@
 set SCONSOPTIONS=%* --silent
 
-set PFX=jptools\secret\knowlec-key201019.pfx
-set PWFILE=jptools\secret\knowlec-key-pass-2019.txt
-@for /F "delims=" %%s in ('type %PWFILE%') do @set PASSWORD=%%s
-del /Q %PWFILE%
-set TIMESERVER=http://timestamp.comodoca.com/authenticode
-
 call miscDepsJp\include\python-jtalk\vcsetup.cmd
 cd /d %~dp0
 cd ..
@@ -16,48 +10,18 @@ call build-and-test.cmd
 @if not "%ERRORLEVEL%"=="0" goto onerror
 cd ..\..
 
-set VERIFYLOG=output\nvda_%VERSION%_verify.log
-del /Q %VERIFYLOG%
-
 call jptools\setupMiscDepsJp.cmd
 
-set PATH="C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64";%PATH%
+@if not "%VERSION%"=="" goto versionready
+for /F "usebackq" %%t in (`jptools\nowdate.cmd`) do set NOWDATE=%%t
+set VERSION=jpdev_%NOWDATE%
+set PUBLISHER=nvdajpdev
+set UPDATEVERSIONTYPE=nvdajpdev
 
-set FILE1=source\synthDrivers\jtalk\libmecab.dll
-@signtool sign /fd sha256 /f %PFX% /p %PASSWORD% /t %TIMESERVER% %FILE1%
-signtool verify /pa %FILE1% >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-timeout /T 5 /NOBREAK
+:versionready
+scons source user_docs launcher publisher=%PUBLISHER% release=1 version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
 
-set FILE2=source\synthDrivers\jtalk\libopenjtalk.dll
-@signtool sign /fd sha256 /f %PFX% /p %PASSWORD% /t %TIMESERVER% %FILE2%
-signtool verify /pa %FILE2% >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-timeout /T 5 /NOBREAK
-
-call jptools\kcCertBuild.cmd
-
-signtool verify /pa dist\lib\%VERSION%\*.dll >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-signtool verify /pa dist\lib64\%VERSION%\*.dll >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-signtool verify /pa dist\libArm64\%VERSION%\*.dll >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-signtool verify /pa dist\*.exe >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-signtool verify /pa output\nvda_%VERSION%.exe >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-
-cd jptools
-call buildControllerClient.cmd
-@if not "%ERRORLEVEL%"=="0" goto onerror
-cd ..
-:skip_client
-
-echo %UPDATEVERSIONTYPE% %VERSION%
 exit /b 0
 
 :onerror
-echo nvdajp build error %ERRORLEVEL%
-@if "%PAUSE%"=="1" pause
 exit /b -1
