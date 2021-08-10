f6cafdf380f97a16d5809ab3614dd4a2d51fe201
Takuya Nishimoto <nishimotz@gmail.com>
2021-08-10 15:29:45 +0900

    #301 sapi4 self.isRunning rather than global

--- source/synthDrivers/sapi4.py
+++ source/synthDrivers/sapi4.py
@@ -164,8 +164,7 @@ class SynthDriver(SynthDriver):
 		textList.append("\\PAU=1\\")
 		text="".join(textList)
 		flags=TTSDATAFLAG_TAGGED
-		global isRunning
-		isRunning = True
+		self.isRunning = True
 		if isPitchCommand:
 			self._ttsCentral.TextData(
 				VOICECHARSET.CHARSET_TEXT,
@@ -186,8 +185,7 @@ class SynthDriver(SynthDriver):
 			)
 
 	def cancel(self):
-		global isRunning
-		isRunning = True
+		self.isRunning = True
 		self._ttsCentral.AudioReset()
 		self.lastIndex=None
 
@@ -201,11 +199,10 @@ class SynthDriver(SynthDriver):
 			self._ttsCentral.AudioResume()
 
 	def setSpeaking(self, switch):
-		global isRunning
-		isRunning = switch
+		self.isRunning = switch
 
 	def isSpeaking(self):
-		return isRunning
+		return self.isRunning
 
 	def removeSetting(self,name):
 		#Putting it here because currently no other synths make use of it. OrderedDict, where you are?
