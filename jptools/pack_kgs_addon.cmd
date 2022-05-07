0efac161defab375026007651fd7cf3b39576a09
Takuya Nishimoto <nishimotz@gmail.com>
2022-05-07 10:39:23 +0900

    KGSVERSION rathar than VERSION https://github.com/nvdajp/nvdajp/issues/217

--- jptools/pack_kgs_addon.cmd
+++ jptools/pack_kgs_addon.cmd
@@ -2,10 +2,10 @@
 @rem > cd jptools
 @rem > pack_kgs_addon.cmd
 cd ..\source
-@rem set VERSION=%nowdate%
-set VERSION=2.0
-py ..\jptools\kgs_manifest.py %VERSION% manifest.ini
+@rem set KGSVERSION=%nowdate%
+set KGSVERSION=2.0
+py ..\jptools\kgs_manifest.py %KGSVERSION% manifest.ini
 7z a ..\jptools\_kgs.zip manifest.ini brailleDisplayDrivers\kgs.py brailleDisplayDrivers\kgsbn46.py brailleDisplayDrivers\brailleMemo.py brailleDisplayDrivers\DirectBM.dll
 del manifest.ini
 cd ..\jptools
-move _kgs.zip kgsbraille-%VERSION%.nvda-addon
+move _kgs.zip kgsbraille-%KGSVERSION%.nvda-addon
