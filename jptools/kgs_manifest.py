f3edd6bf68dc0e642c00a457999bc74b10b123cd
Takuya Nishimoto <nishimotz@gmail.com>
2022-05-07 09:23:18 +0900

    kgsbraille-2.0 https://github.com/nvdajp/nvdajp/issues/217

--- jptools/kgs_manifest.py
+++ jptools/kgs_manifest.py
@@ -1,17 +1,17 @@
 from __future__ import unicode_literals, print_function
 import argparse
 ap = argparse.ArgumentParser()
-ap.add_argument('nowdate')
+ap.add_argument('version')
 ap.add_argument('fileName')
 args = ap.parse_args()
 with open(args.fileName, 'w') as f:
     f.write(
         """name = kgsbraille
 summary = "KGS Braille Memo Driver"
-version = {nowdate}
+version = {version}
 author = "Takuya Nishimoto <nishimotz@gmail.com>"
 description = "KGS Driver, which supports BM Smart series, Braille Memo series, Braille Memo Pocket and Braille Tender."
 url = http://www.nvda.jp/en/
 minimumNVDAVersion = 2019.3.0
 lastTestedNVDAVersion = 2022.1.0
-""".format(nowdate=args.nowdate))
+""".format(version=args.version))
--- jptools/pack_kgs_addon.cmd
+++ jptools/pack_kgs_addon.cmd
@@ -2,8 +2,10 @@
 @rem > cd jptools
 @rem > pack_kgs_addon.cmd
 cd ..\source
-py ..\jptools\kgs_manifest.py %nowdate% manifest.ini
+@rem set VERSION=%nowdate%
+set VERSION=2.0
+py ..\jptools\kgs_manifest.py %VERSION% manifest.ini
 7z a ..\jptools\_kgs.zip manifest.ini brailleDisplayDrivers\kgs.py brailleDisplayDrivers\kgsbn46.py brailleDisplayDrivers\brailleMemo.py brailleDisplayDrivers\DirectBM.dll
 del manifest.ini
 cd ..\jptools
-move _kgs.zip kgsbraille-%nowdate%.nvda-addon
+move _kgs.zip kgsbraille-%VERSION%.nvda-addon
