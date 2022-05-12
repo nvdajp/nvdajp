1a1e6837d9e65e45fdc7512cc7970d0131ddfb96
Takuya Nishimoto <nishimotz@gmail.com>
2022-05-12 14:43:15 +0900

    KGSVERSION

--- jptools/pack_kgs_addon.cmd
+++ jptools/pack_kgs_addon.cmd
@@ -2,8 +2,8 @@
 @rem > cd jptools
 @rem > pack_kgs_addon.cmd
 cd ..\source
-@rem set KGSVERSION=%nowdate%
-set KGSVERSION=2.0
+set KGSVERSION=%nowdate%
+@rem set KGSVERSION=2.0
 py ..\jptools\kgs_manifest.py %KGSVERSION% manifest.ini
 7z a ..\jptools\_kgs.zip manifest.ini brailleDisplayDrivers\kgs.py brailleDisplayDrivers\brailleMemo.py brailleDisplayDrivers\DirectBM.dll
 del manifest.ini
