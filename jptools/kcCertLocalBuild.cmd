d2f9b83219fd0180c38b769e52eafd358f20da2b
Takuya Nishimoto <nishimotz@gmail.com>
2022-08-24 21:29:40 +0900

    shuaruta-key-2022

--- jptools/kcCertLocalBuild.cmd
+++ jptools/kcCertLocalBuild.cmd
@@ -5,7 +5,7 @@ for /F "usebackq" %%t in (`jptools\nowdate.cmd`) do set NOWDATE=%%t
 set VERSION=%VERSION%_%NOWDATE%
 set UPDATEVERSIONTYPE=%UPDATEVERSIONTYPE%alpha
 set PUBLISHER=nvdajp
-copy c:\work\kc\pfx\knowlec-key-pass-2021.txt jptools\secret
-copy c:\work\kc\pfx\knowlec-key211018.pfx jptools\secret
+copy c:\work\kc\pfx\shuaruta-key-pass-2022.txt jptools\secret
+copy c:\work\kc\pfx\shuaruta-key220824.pfx jptools\secret
 del source\_buildVersion.py*
 call jptools\kcCertAllBuild.cmd
