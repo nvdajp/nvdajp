a9d1424a6b2109fff884d88d2a15489de5a35ca2
Takuya Nishimoto <nishimotz@gmail.com>
2023-02-14 10:51:54 +0900

    update addon manifests

--- jptools/jtalk_manifest.py
+++ jptools/jtalk_manifest.py
@@ -13,5 +13,5 @@ author = "Takuya Nishimoto <nishimotz@gmail.com>"
 description = "Japanese speech engine for NVDA, based on Open JTalk, MeCab and MMDAgent."
 url = http://www.nvda.jp/en/
 minimumNVDAVersion = 2014.1.0
-lastTestedNVDAVersion = 2022.1.0
+lastTestedNVDAVersion = 2023.1.0
 """.format(nowdate=args.nowdate))
