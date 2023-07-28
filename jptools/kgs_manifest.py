320f814f6c15b74718363655498b69b8c7e67bce
Takuya Nishimoto <nishimotz@gmail.com>
2023-07-28 15:17:24 +0900

    kgsbraille-2.1

--- jptools/kgs_manifest.py
+++ jptools/kgs_manifest.py
@@ -1,4 +1,3 @@
-from __future__ import unicode_literals, print_function
 import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument('version')
@@ -9,7 +8,7 @@ with open(args.fileName, 'w') as f:
         """name = kgsbraille
 summary = "KGS Braille Memo Driver"
 version = {version}
-author = "Takuya Nishimoto <nishimotz@gmail.com>"
+author = "Takuya Nishimoto <nishimotz@gmail.com> / Shuaruta Inc."
 description = "KGS Driver, which supports BM Smart series, Braille Memo series, Braille Memo Pocket and Braille Tender."
 url = https://www.nvda.jp/en/
 minimumNVDAVersion = 2019.3.0
