3311b247122c541cd959c6b061789c51d76f68ba
Takuya Nishimoto <nishimotz@gmail.com>
2023-08-08 23:17:11 +0900

    https://github.com/nvdajp/nvdajp/issues/389

---
+++ jptools/nonCertBuild2a.cmd
@@ -0,0 +1,24 @@
+set SCONSOPTIONS=%*
+
+@if not "%VERSION%"=="" goto versionready
+for /F "usebackq" %%t in (`jptools\nowdate.cmd`) do set NOWDATE=%%t
+set VERSION=jpdev_%NOWDATE%
+set PUBLISHER=nvdajpdev
+set UPDATEVERSIONTYPE=nvdajpdev
+
+:versionready
+set OPTIONS=publisher=%PUBLISHER% version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% %SCONSOPTIONS%
+set OPTIONS=%OPTIONS% release=1
+call scons source %OPTIONS%
+@if not "%ERRORLEVEL%"=="0" goto onerror
+@REM call scons user_docs %OPTIONS%
+@REM @if not "%ERRORLEVEL%"=="0" goto onerror
+@REM call scons dist %OPTIONS%
+@REM @if not "%ERRORLEVEL%"=="0" goto onerror
+@REM call scons launcher %OPTIONS%
+@REM @if not "%ERRORLEVEL%"=="0" goto onerror
+
+exit /b 0
+
+:onerror
+exit /b -1
