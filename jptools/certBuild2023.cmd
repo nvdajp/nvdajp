5a29f594389cc32b4c7542062833f14b773b2671
Takuya Nishimoto <nishimotz@gmail.com>
2023-08-28 17:32:39 +0900

    [skip ci]

--- jptools/certBuild2023.cmd
+++ jptools/certBuild2023.cmd
@@ -6,43 +6,48 @@ call miscDepsJp\include\python-jtalk\vcsetup.cmd
 cd /d %~dp0
 cd ..
 
-if not exist output mkdir output
-set VERIFYLOG=output\nvda_%VERSION%_verify.log
-del /Q %VERIFYLOG%
-
 call jptools\setupMiscDepsJp.cmd
 
 set SIGNTOOL="C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x64\signtool.exe"
 
-set FILE1=source\synthDrivers\jtalk\libmecab.dll
-%SIGNTOOL% sign /a /fd SHA256 /tr %TIMESERVER% /td SHA256 %FILE1%
-%SIGNTOOL% verify /pa %FILE1% >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
+%SIGNTOOL% sign /a /fd SHA256 /tr %TIMESERVER% /td SHA256 source\synthDrivers\jtalk\libmecab.dll
 timeout /T 5 /NOBREAK
 
-set FILE2=source\synthDrivers\jtalk\libopenjtalk.dll
-%SIGNTOOL% sign /a /fd SHA256 /tr %TIMESERVER% /td SHA256 %FILE2%
-%SIGNTOOL% verify /pa %FILE2% >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
+%SIGNTOOL% sign /a /fd SHA256 /tr %TIMESERVER% /td SHA256 source\synthDrivers\jtalk\libopenjtalk.dll
 timeout /T 5 /NOBREAK
 
 call scons.bat source user_docs launcher release=1 certTimestampServer=%TIMESERVER% publisher=%PUBLISHER% version=%VERSION% updateVersionType=%UPDATEVERSIONTYPE% --silent %SCONSOPTIONS%
 @if not "%ERRORLEVEL%"=="0" goto onerror
 
-%SIGNTOOL% verify /pa output\nvda_%VERSION%.exe >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-%SIGNTOOL% verify /pa dist\*.exe >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-%SIGNTOOL% verify /pa dist\lib\%VERSION%\*.dll >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-%SIGNTOOL% verify /pa dist\lib64\%VERSION%\*.exe >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-%SIGNTOOL% verify /pa dist\lib64\%VERSION%\*.dll >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-%SIGNTOOL% verify /pa dist\libArm64\%VERSION%\*.exe >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-%SIGNTOOL% verify /pa dist\libArm64\%VERSION%\*.dll >> %VERIFYLOG%
-@if not "%ERRORLEVEL%"=="0" goto onerror
+set VERIFYLOG=output\nvda_%VERSION%_verify.log
+del /Q %VERIFYLOG%
+
+for /r "output" %%i in (*.exe) do (
+    signtool verify /pa "%%i" >> %VERIFYLOG%
+    @if not "%ERRORLEVEL%"=="0" goto onerror
+)
+for /r "dist" %%i in (*.exe) do (
+    signtool verify /pa "%%i" >> %VERIFYLOG%
+    @if not "%ERRORLEVEL%"=="0" goto onerror
+)
+for /r "dist\synthDrivers\jtalk" %%i in (*.dll *.exe) do (
+    signtool verify /pa "%%i" >> %VERIFYLOG%
+    @if not "%ERRORLEVEL%"=="0" goto onerror
+)
+for /r "dist\lib" %%i in (*.dll *.exe) do (
+    if "%%~nxi" neq "Microsoft.UI.UIAutomation.dll" (
+        signtool verify /pa "%%i" >> %VERIFYLOG%
+        @if not "%ERRORLEVEL%"=="0" goto onerror
+    )
+)
+for /r "dist\lib64" %%i in (*.dll *.exe) do (
+    signtool verify /pa "%%i" >> %VERIFYLOG%
+    @if not "%ERRORLEVEL%"=="0" goto onerror
+)
+for /r "dist\libArm64" %%i in (*.dll *.exe) do (
+    signtool verify /pa "%%i" >> %VERIFYLOG%
+    @if not "%ERRORLEVEL%"=="0" goto onerror
+)
 
 echo %UPDATEVERSIONTYPE% %VERSION%
 exit /b 0
