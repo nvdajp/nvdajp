b9c732731aa32d3c9ad8446ed877ab48af2a826d
Takuya Nishimoto <nishimotz@gmail.com>
2023-10-15 20:21:35 +0900

    文字説明チェック Python 3 対応

--- jpchar/checkSymbols.py
+++ jpchar/checkSymbols.py
@@ -1,15 +1,15 @@
 # coding: UTF-8
 #checkSymbols.py
 #A part of NonVisual Desktop Access (NVDA)
-#Copyright (C) 2012-2016 Takuya Nishimoto (NVDA Japanese Team)
+#Copyright (C) 2012-2016,2023 Takuya Nishimoto, NVDA Japanese Team, Shuaruta Inc.
 # usage:
 # > cd jpchar
-# > python checkSymbols.py
+# > py -3 checkSymbols.py
 
 #import sys
 #sys.path.append(r'..\source\synthDrivers\jtalk')
 from _checkCharDesc import *
-sy = read_symbol_file(SY_FILE)
+sy = read_symbol_file(SY_FILE, raiseDuplicated=False)
 #ch = read_chardesc_file(CH_FILE)
 ch = read_characters_file(CS_FILE, use_both=True)
 print_different(sy, ch, skip_included=True)
