c098b2a8152afcfe630147e34c70457a61f07856
Takuya Nishimoto <nishimotz@gmail.com>
2023-11-25 17:18:36 +0900

    jptools/buildControllerClient を 2024.1 の仕様にする

--- jptools/buildControllerClient.cmd
+++ jptools/buildControllerClient.cmd
@@ -2,15 +2,25 @@ SET ARGS=publisher=nvdajp release=1 version=%VERSION%
 call scons nvdaHelper\client %ARGS%
 cd jptools
 cd nvdajpClient
-copy ..\..\build\x86\client\nvdaController.h client
-copy ..\..\build\x86\client\nvdaControllerClient32.dll client
-copy ..\..\build\x86\client\nvdaControllerClient32.dll.pdb client
-copy ..\..\build\x86\client\nvdaControllerClient32.exp client
-copy ..\..\build\x86\client\nvdaControllerClient32.lib client
-copy ..\..\build\x86_64\client\nvdaControllerClient64.dll client
-copy ..\..\build\x86_64\client\nvdaControllerClient64.dll.pdb client
-copy ..\..\build\x86_64\client\nvdaControllerClient64.exp client
-copy ..\..\build\x86_64\client\nvdaControllerClient64.lib client
+
+copy ..\..\build\x86\client\nvdaController.h client\x86
+copy ..\..\build\x86\client\nvdaControllerClient.dll client\x86
+copy ..\..\build\x86\client\nvdaControllerClient.dll.pdb client\x86
+copy ..\..\build\x86\client\nvdaControllerClient.exp client\x86
+copy ..\..\build\x86\client\nvdaControllerClient.lib client\x86
+
+copy ..\..\build\x86_64\client\nvdaController.h client\x64
+copy ..\..\build\x86_64\client\nvdaControllerClient.dll client\x64
+copy ..\..\build\x86_64\client\nvdaControllerClient.dll.pdb client\x64
+copy ..\..\build\x86_64\client\nvdaControllerClient.exp client\x64
+copy ..\..\build\x86_64\client\nvdaControllerClient.lib client\x64
+
+copy ..\..\build\arm64\client\nvdaController.h client\arm64
+copy ..\..\build\arm64\client\nvdaControllerClient.dll client\arm64
+copy ..\..\build\arm64\client\nvdaControllerClient.dll.pdb client\arm64
+copy ..\..\build\arm64\client\nvdaControllerClient.exp client\arm64
+copy ..\..\build\arm64\client\nvdaControllerClient.lib client\arm64
+
 SET OUTFILE=..\..\output\nvda_%VERSION%_controllerClientJp.zip
 del /Q %OUTFILE%
 7z a %OUTFILE% client python license.txt readme.html readmejp.txt
