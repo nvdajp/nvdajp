36a320567f036403d8b966aff96c0cc942504539
Takuya Nishimoto <nishimotz@gmail.com>
2023-11-25 18:09:18 +0900

    jptools/buildControllerClient修正

--- jptools/buildControllerClient.cmd
+++ jptools/buildControllerClient.cmd
@@ -3,26 +3,26 @@ call scons nvdaHelper\client %ARGS%
 cd jptools
 cd nvdajpClient
 
-copy ..\..\build\x86\client\nvdaController.h client\x86
-copy ..\..\build\x86\client\nvdaControllerClient.dll client\x86
-copy ..\..\build\x86\client\nvdaControllerClient.dll.pdb client\x86
-copy ..\..\build\x86\client\nvdaControllerClient.exp client\x86
-copy ..\..\build\x86\client\nvdaControllerClient.lib client\x86
+copy ..\..\build\x86\client\nvdaController.h x86
+copy ..\..\build\x86\client\nvdaControllerClient.dll x86
+copy ..\..\build\x86\client\nvdaControllerClient.dll.pdb x86
+copy ..\..\build\x86\client\nvdaControllerClient.exp x86
+copy ..\..\build\x86\client\nvdaControllerClient.lib x86
 
-copy ..\..\build\x86_64\client\nvdaController.h client\x64
-copy ..\..\build\x86_64\client\nvdaControllerClient.dll client\x64
-copy ..\..\build\x86_64\client\nvdaControllerClient.dll.pdb client\x64
-copy ..\..\build\x86_64\client\nvdaControllerClient.exp client\x64
-copy ..\..\build\x86_64\client\nvdaControllerClient.lib client\x64
+copy ..\..\build\x86_64\client\nvdaController.h x64
+copy ..\..\build\x86_64\client\nvdaControllerClient.dll x64
+copy ..\..\build\x86_64\client\nvdaControllerClient.dll.pdb x64
+copy ..\..\build\x86_64\client\nvdaControllerClient.exp x64
+copy ..\..\build\x86_64\client\nvdaControllerClient.lib x64
 
-copy ..\..\build\arm64\client\nvdaController.h client\arm64
-copy ..\..\build\arm64\client\nvdaControllerClient.dll client\arm64
-copy ..\..\build\arm64\client\nvdaControllerClient.dll.pdb client\arm64
-copy ..\..\build\arm64\client\nvdaControllerClient.exp client\arm64
-copy ..\..\build\arm64\client\nvdaControllerClient.lib client\arm64
+copy ..\..\build\arm64\client\nvdaController.h arm64
+copy ..\..\build\arm64\client\nvdaControllerClient.dll arm64
+copy ..\..\build\arm64\client\nvdaControllerClient.dll.pdb arm64
+copy ..\..\build\arm64\client\nvdaControllerClient.exp arm64
+copy ..\..\build\arm64\client\nvdaControllerClient.lib arm64
 
 SET OUTFILE=..\..\output\nvda_%VERSION%_controllerClientJp.zip
 del /Q %OUTFILE%
-7z a %OUTFILE% client python license.txt readme.html readmejp.txt
+7z a %OUTFILE% arm64 examples x64 x86 license.txt readme.html readmejp.txt
 cd ..
 cd ..
