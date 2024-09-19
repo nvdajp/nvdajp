6532a0f57c520a100dd78b4a0d0f35eb7f21754c
Takuya Nishimoto <nishimotz@gmail.com>
2024-09-19 11:50:01 +0900

    devbuild [skip ci]

--- jptools/devbuild2024.cmd
+++ jptools/devbuild2024.cmd
@@ -1,4 +1,10 @@
 set VERSION=2024.4jp
+set VERSION_BUILD=99999
 del source\_buildVersion.py
-venvUtils\venvCmd jptools\devbuild.cmd source version_build=99999 --all-cores
-rununittests.bat
+call venvUtils\venvCmd jptools\devbuild.cmd source version_build=%VERSION_BUILD% --all-cores
+@if not "%ERRORLEVEL%"=="0" goto onerror
+call rununittests.bat
+exit /b 0
+:onerror
+echo error %ERRORLEVEL%
+exit /b -1
