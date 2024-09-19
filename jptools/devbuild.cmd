23be07299649a6f2653efc83a2576bfa773f9b52
Takuya Nishimoto <nishimotz@gmail.com>
2024-09-19 09:38:36 +0900

    Revert "devbuild [skip ci]"
    This reverts commit e1a8482816e44a43c14cf1c41e1ec92e1e0cf33c.

--- jptools/devbuild.cmd
+++ jptools/devbuild.cmd
@@ -1,53 +1,9 @@
 @rem usage:
-@rem set VERSION=2024.4jp
-@rem jptools\devbuild.cmd source dist launcher user_docs pot symbolsArchive version_build=99999 -j12
+@rem del source\_buildVersion.py
+@rem jptools\devbuild.cmd
+@rem example: jptools\devbuild.cmd source dist launcher user_docs pot symbolsArchive -j12
 
-@echo off
-setlocal enabledelayedexpansion
-
-rem Usage:
-rem set VERSION=2024.4jp
-rem jptools\devbuild.cmd source dist launcher user_docs pot symbolsArchive version_build=99999 -j12
-
-rem Delete source_buildVersion.py if it exists
-if exist "source_buildVersion.py" (
-    del "source_buildVersion.py" || (
-        echo Failed to delete source_buildVersion.py
-        exit /b 1
-    )
-)
-
-rem Set default VERSION if not defined
-if not defined VERSION (
-    set "VERSION=2024.4jp"
-)
-
-rem Initialize variables
-set "SCONSARGS="
-set "version_build_set=false"
-
-rem Parse arguments
-for %%A in (%*) do (
-    echo %%A | findstr /b /c:"version_build=" >nul
-    if not errorlevel 1 (
-        set "version_build=%%A"
-        set "version_build_set=true"
-    ) else (
-        set "SCONSARGS=!SCONSARGS! %%A"
-    )
-)
-
-rem Append version_build if not set
-if "!version_build_set!"=="false" (
-    set "SCONSARGS=!SCONSARGS! version_build=99999"
-) else (
-    echo version_build=!version_build!
-)
-
-rem Continue with the rest of your script using !SCONSARGS!
-echo Final SCONSARGS: !SCONSARGS!
-
-endlocal
+set SCONSARGS=%*
 
 call miscDepsJp\include\python-jtalk\vcsetup.cmd
 cd /d %~dp0
@@ -84,9 +40,6 @@ cd ..\..
 call jptools\setupMiscDepsJp.cmd
 
 call scons.bat %SCONSARGS%
-@if not "%ERRORLEVEL%"=="0" goto onerror
-
-call rununittests.bat
 @if not "%ERRORLEVEL%"=="0" goto onerror
 
 exit /b 0
