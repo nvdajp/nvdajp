dd1f5e94aa25b97c9a6da93dbca34a1fbdce4a06
Takuya Nishimoto <nishimotz@gmail.com>
2024-12-04 17:50:20 +0900

    WAIC-CODE-0029-02

--- tests/system/robot/chromeTests.py
+++ tests/system/robot/chromeTests.py
@@ -2879,3 +2879,19 @@ def test_waic_as_0029_01():
 		actualSpeech,
 		"閉じる  button  このウィンドウを閉じると、入力された情報は破棄され、メインページに戻ります ideographic period",
 	)
+
+
+def test_waic_as_0029_02():
+	_chrome.prepareChrome("""
+	<iframe width="800" height="600" src="https://waic.github.io/as_test/WAIC-CODE/WAIC-CODE-0029-02.html"></iframe>
+	""")
+	actualSpeech = _chrome.getSpeechAfterKey("downArrow")
+	_asserts.strings_match(
+		actualSpeech,
+		"frame  link  メインページへ戻る",
+	)
+	actualSpeech = _chrome.getSpeechAfterTab()
+	_asserts.strings_match(
+		actualSpeech,
+		"名前  edit  aria-describedbyでリンクされたこの分野のちょっとした指示です ideographic period  blank\nFocus mode",
+	)
--- tests/system/robot/chromeTests.robot
+++ tests/system/robot/chromeTests.robot
@@ -172,3 +172,6 @@ aria-errormessage
 test_waic_as_0029_01
 	[Documentation]	aria-describedby 属性による説明ラベルの提供 (button要素 : aria-label属性と併用)
 	test waic as 0029 01
+test_waic_as_0029_02
+	[Documentation]	aria-describedby 属性による説明ラベルの提供 (input要素)
+	test waic as 0029 02
