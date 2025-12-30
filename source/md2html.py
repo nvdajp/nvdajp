070765f96cb1375cad906ac9834c8e52c08e957f
Takuya Nishimoto <nishimotz@gmail.com>
2025-12-30 19:38:31 +0900

    Add readmejp docType support to md2html.py [skip ci]

--- source/md2html.py
+++ source/md2html.py
@@ -163,8 +163,11 @@ def main(source: str, dest: str, lang: str = "en", docType: str | None = None):
 	isDevGuide = docType == "developerGuide"
 	isChanges = docType == "changes"
 	isKeyCommands = docType == "keyCommands"
-	if docType and not any([isUserGuide, isDevGuide, isChanges, isKeyCommands]):
+	# BEGIN JP PATCH (readmejp support)
+	isReadmejp = docType == "readmejp"
+	if docType and not any([isUserGuide, isDevGuide, isChanges, isKeyCommands, isReadmejp]):
 		raise ValueError(f"Unknown docType {docType}")
+	# END JP PATCH
 	with open(source, "r", encoding="utf-8") as mdFile:
 		mdStr = mdFile.read()
 
@@ -176,6 +179,10 @@ def main(source: str, dest: str, lang: str = "en", docType: str | None = None):
 		extraStylesheet = '<link rel="stylesheet" href="numberedHeadings.css">'
 	elif isChanges or isKeyCommands:
 		extraStylesheet = ""
+	# BEGIN JP PATCH (readmejp support)
+	elif isReadmejp:
+		extraStylesheet = '<link rel="stylesheet" href="numberedHeadings.css">'
+	# END JP PATCH
 	else:
 		raise ValueError(f"Unknown target type for {dest}")
 
@@ -214,7 +221,9 @@ if __name__ == "__main__":
 		"--docType",
 		help="Type of document",
 		action="store",
-		choices=["userGuide", "developerGuide", "changes", "keyCommands"],
+		# BEGIN JP PATCH (readmejp support)
+		choices=["userGuide", "developerGuide", "changes", "keyCommands", "readmejp"],
+		# END JP PATCH
 	)
 	args.add_argument("source", help="Path to the markdown file")
 	args.add_argument("dest", help="Path to the resulting html file")
