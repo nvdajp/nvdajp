4464e9b0ea084014184890d9890c1d489de21c6f
Takuya Nishimoto <nishimotz@gmail.com>
2025-12-30 22:34:09 +0900

    fix: update unit tests to support TABLES_DIR_JP for Japanese braille tables

--- tests/unit/test_brailleTables.py
+++ tests/unit/test_brailleTables.py
@@ -18,9 +18,15 @@ class TestFBrailleTables(unittest.TestCase):
 		"""Tests whether all defined tables exist."""
 		tables = brailleTables.listTables()
 		for table in tables:
+			# BEGIN JP PATCH (Support TABLES_DIR_JP for Japanese tables)
+			if table.source == brailleTables.TableSource.BUILTIN_JP:
+				tableDir = brailleTables.TABLES_DIR_JP
+			else:
+				tableDir = brailleTables._tablesDirs.get(table.source, brailleTables.TABLES_DIR)
+			# END JP PATCH
 			self.assertTrue(
-				os.path.isfile(os.path.join(brailleTables.TABLES_DIR, table.fileName)),
-				msg="{table} table not found".format(table=table.displayName),
+				os.path.isfile(os.path.join(tableDir, table.fileName)),
+				msg="{table} table not found in {dir}".format(table=table.displayName, dir=tableDir),
 			)
 
 	def test_renamedTableExistence(self):
