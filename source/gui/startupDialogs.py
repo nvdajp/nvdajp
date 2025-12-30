4a83394d3b60d83a8e3233472e4d9ded8c3bf209
Takuya Nishimoto <nishimotz@gmail.com>
2025-12-30 22:22:40 +0900

    feat: restore Japanese UI elements from betajp branch [skip ci]

--- source/gui/startupDialogs.py
+++ source/gui/startupDialogs.py
@@ -75,6 +75,24 @@ class WelcomeDialog(
 			self.kbdList.SetSelection(index)
 		except (ValueError, KeyError):
 			log.error("Could not set Keyboard layout list to current layout", exc_info=True)
+		# BEGIN JP PATCH (Japanese keyboard modifier keys)
+		# Translators: The label of a checkbox in the Welcome dialog.
+		nconvAsNVDAModifierText = _("Use NonConvert as an NVDA modifier key")
+		self.nconvAsNVDAModifierCheckBox = sHelper.addItem(
+			wx.CheckBox(optionsBox, label=nconvAsNVDAModifierText)
+		)
+		self.nconvAsNVDAModifierCheckBox.SetValue(config.conf["keyboard"]["useNonConvertAsNVDAModifierKey"])
+		# Translators: The label of a checkbox in the Welcome dialog.
+		convAsNVDAModifierText = _("Use Convert as an NVDA modifier key")
+		self.convAsNVDAModifierCheckBox = sHelper.addItem(
+			wx.CheckBox(optionsBox, label=convAsNVDAModifierText)
+		)
+		self.convAsNVDAModifierCheckBox.SetValue(config.conf["keyboard"]["useConvertAsNVDAModifierKey"])
+		# Translators: The label of a checkbox in the Welcome dialog.
+		escAsNVDAModifierText = _("Use Escape as an NVDA modifier key")
+		self.escAsNVDAModifierCheckBox = sHelper.addItem(wx.CheckBox(optionsBox, label=escAsNVDAModifierText))
+		self.escAsNVDAModifierCheckBox.SetValue(config.conf["keyboard"]["useEscapeAsNVDAModifierKey"])
+		# END JP PATCH
 		# Translators: The label of a checkbox in the Welcome dialog.
 		capsAsNVDAModifierText = _("&Use CapsLock as an NVDA modifier key")
 		self.capsAsNVDAModifierCheckBox = sHelper.addItem(
@@ -130,6 +148,13 @@ class WelcomeDialog(
 			)
 		else:
 			config.conf["keyboard"]["NVDAModifierKeys"] = NVDAKeysVal
+		# BEGIN JP PATCH (Japanese keyboard modifier keys)
+		config.conf["keyboard"]["useNonConvertAsNVDAModifierKey"] = (
+			self.nconvAsNVDAModifierCheckBox.IsChecked()
+		)
+		config.conf["keyboard"]["useConvertAsNVDAModifierKey"] = self.convAsNVDAModifierCheckBox.IsChecked()
+		config.conf["keyboard"]["useEscapeAsNVDAModifierKey"] = self.escAsNVDAModifierCheckBox.IsChecked()
+		# END JP PATCH
 		if self.startAfterLogonCheckBox.Enabled:
 			config.setStartAfterLogon(self.startAfterLogonCheckBox.Value)
 		config.conf["general"]["showWelcomeDialogAtStartup"] = (
@@ -286,6 +311,10 @@ class AskAllowUsageStatsDialog(
 			"Please refer to the User Guide for a current list of all data collected.\n\n"
 			"Do you wish to allow NV Access to periodically collect this data in order to improve NVDA?",
 		)
+		# BEGIN JP PATCH (Replace "NV Access" with "NVDA Japanese Team")
+		# Translators: 'NV Access' should be replaced with 'NVDA Japanese Team'
+		message = message.replace("NV Access", _("NVDA Japanese Team"))
+		# END JP PATCH
 		sText = sHelper.addItem(wx.StaticText(self, label=message))
 		# the wx.Window must be constructed before we can get the handle.
 		import windowUtils
