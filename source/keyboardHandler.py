34352985b9c1833ceaf27e9523d0272d4ed0e501
Takuya Nishimoto <nishimotz@gmail.com>
2026-01-03 22:03:17 +0900

    Enhance Japanese input method support in NVDA
    - Added new key mappings for IME change status to improve input method functionality.
    - Implemented additional checks for non-convert and convert keys as NVDAModifierKeys, enhancing compatibility with Japanese keyboard layouts.
    - Introduced reporting of key down events for Japanese input, allowing for better handling of user gestures.
    - Updated input composition handling to support discriminant reading and improved braille output for Japanese characters.
    
    These changes aim to enhance the user experience for Japanese-speaking users by providing better input method support and accessibility features.

--- source/keyboardHandler.py
+++ source/keyboardHandler.py
@@ -111,6 +111,16 @@ def isNVDAModifierKey(vkCode: int, extended: bool) -> bool:
 		return True
 	elif (config.conf["keyboard"]["NVDAModifierKeys"] & NVDAKey.CAPS_LOCK) and vkCode == winUser.VK_CAPITAL:
 		return True
+	elif config.conf["keyboard"]["useNonConvertAsNVDAModifierKey"] and (
+		vkCode == winUser.VK_NONCONVERT or (vkCode == winUser.VK_IME_OFF and extended)
+	):  # nvdajp
+		return True
+	elif config.conf["keyboard"]["useConvertAsNVDAModifierKey"] and (
+		vkCode == winUser.VK_CONVERT or (vkCode == winUser.VK_IME_ON and extended)
+	):  # nvdajp
+		return True
+	elif config.conf["keyboard"]["useEscapeAsNVDAModifierKey"] and vkCode == winUser.VK_ESCAPE:  # nvdajp
+		return True
 	else:
 		return False
 
@@ -316,6 +326,12 @@ def internal_keyDownEvent(vkCode, scanCode, extended, injected):
 			if res > 0:
 				for ch in charBuf[:res]:
 					eventHandler.queueEvent("typedCharacter", focus, ch=ch)
+	# nvdajp begin
+	if config.conf["keyboard"]["nvdajpEnableKeyEvents"]:
+		from NVDAObjects import inputComposition
+
+		inputComposition.reportKeyDownEvent(gesture)
+	# nvdajp end
 	return True
 
 
