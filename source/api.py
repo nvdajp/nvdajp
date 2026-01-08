79000c8d8c10f91d1b3c95bb7c99211441cdc64c
Takuya Nishimoto <nishimotz@gmail.com>
2026-01-08 21:48:48 +0900

    Fix copyright header in source/api.py: update to 2025 and add hwf1324
    This was reverted during the 504e956 merge conflict resolution.
    The correct version should match nvaccess/beta.

--- source/api.py
+++ source/api.py
@@ -1,6 +1,6 @@
 # A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2006-2022 NV Access Limited, James Teh, Michael Curran, Peter Vagner, Derek Riemer,
-# Davy Kager, Babbage B.V., Leonard de Ruijter, Joseph Lee, Accessolutions, Julien Cochuyt
+# Copyright (C) 2006-2025 NV Access Limited, James Teh, Michael Curran, Peter Vagner, Derek Riemer,
+# Davy Kager, Babbage B.V., Leonard de Ruijter, Joseph Lee, Accessolutions, Julien Cochuyt, hwf1324
 # This file may be used under the terms of the GNU General Public License, version 2 or later.
 # For more details see: https://www.gnu.org/licenses/gpl-2.0.html
 
